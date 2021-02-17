babf26fcd577dd2549d795d95aed2f3b2e7db086
Ivan Nikolsky <enty8080@gmail.com>
2021-02-17 20:37:13 +0300

    Create colors.py

---
+++ core/colors.py
@@ -0,0 +1,23 @@
+class colors:
+    def __init__(self):
+        self.BLACK = '\033[30m'
+        self.RED = '\033[31m'
+        self.GREEN = '\033[32m'
+        self.YELLOW = '\033[33m'
+        self.BLUE = '\033[34m'
+        self.PURPLE = '\033[35m'
+        self.CYAN = '\033[36m'
+        self.WHITE = '\033[77m'
+
+        self.END = '\033[0m'
+        self.BOLD = '\033[1m'
+        self.DARK = '\033[2m'
+        self.BENT = '\033[3m'
+        self.LINE = '\033[4m'
+        self.TWINK = '\033[5m'
+        self.BACK = '\033[7m'
+        
+        self.REMOVE = '\033[1K\r'
+        self.CLEAR = '\033c'
+        
+        self.NEWLINE = '\n'
