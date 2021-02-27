ccfcb8c2786b34cb9bfbfdeef163afd90d31e4b3
Ivan Nikolsky <enty8080@gmail.com>
2021-02-27 17:51:55 +0300

    Add files via upload

---
+++ data/modules/exploit/multi/stager/shell_reverse_tcp/core/session.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2021 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from core.storage import local_storage
+from core.badges import badges
+from core.exceptions import exceptions
+
+class session:
+    def __init__(self):
+        self.local_storage = local_storage()
+        self.badges = badges()
+        self.exceptions = exceptions
+
+    def get_session(self, session):
+        sessions = None
+        multi_sessions = self.local_storage.get("post/multi/shell")
+        if sessions:
+            if int(session) in sessions.keys():
+                return (True, sessions[int(session)])
+        if multi_sessions:
+            if int(session) in multi_sessions.keys():
+                return (True, multi_sessions[int(session)])
+        self.badges.output_error("Invalid session given!")
+        return (False, None)
