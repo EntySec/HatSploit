14170c3c2c08d9d23ebc4ab3b34d6c3afcbfb094
Ivan Nikolsky <enty8080@gmail.com>
2021-02-28 13:24:00 +0300

    Update download.py

--- modules/post/macos/membrane/transfer/download.py
+++ modules/post/macos/membrane/transfer/download.py
@@ -69,6 +69,6 @@ class HatSploitModule:
 
     def run(self):
         lpath, rpath, session = self.parser.parse_options(self.options)
-        exists, controller = self.sessions.get_session("macos/membrane", session)
-        if exists:
-            controller.download(rpath, lpath)
+        session = self.sessions.get_session("macos/membrane", session)
+        if session:
+            session.download(rpath, lpath)
