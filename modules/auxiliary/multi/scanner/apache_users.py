45f079859e6695dab5f1fdb769dc35481b37c27c
Ivan Nikolsky <enty8080@gmail.com>
2021-03-06 21:24:26 +0300

    Update apache_users.py

--- modules/auxiliary/multi/scanner/apache_users.py
+++ modules/auxiliary/multi/scanner/apache_users.py
@@ -65,7 +65,9 @@ class HatSploitModule:
             'URL': {
                 'Description': "Target URL address.",
                 'Value': None,
-                'Type': str,
+                'Types': [
+                    str
+                ],
                 'Required': True
             }
         }
