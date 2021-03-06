a469fabede85f1db12d3aa5add81f2ef1d33e5ec
Ivan Nikolsky <enty8080@gmail.com>
2021-03-06 23:24:38 +0300

    Update command.py

--- core/templates/command.py
+++ core/templates/command.py
@@ -24,61 +24,16 @@
 # SOFTWARE.
 #
 
-from core.cli.badges import badges
-from core.cli.parser import parser
-from core.cli.tables import tables
+from core.templates.hsf import HatSploit
 
-from core.base.jobs import jobs
-from core.base.config import config
-from core.base.sessions import sessions
-from core.base.execute import execute
-from core.base.exceptions import exceptions
-from core.base.storage import local_storage
-from core.base.storage import global_storage
-
-from core.modules.modules import modules
-from core.plugins.plugins import plugins
-
-from utils.adb_tools import adb_tools
-from utils.fsmanip import fsmanip
-from utils.hatvenom import hatvenom
-from utils.web_tools import web_tools
-from utils.tcp_tools import tcp_tools
-from utils.pseudo_shell import pseudo_shell
-from utils.string_tools import string_tools
-
-class HatSploitCommand:
-    def __init__(self):
-        self.badges = badges()
-        self.parser = parser()
-        self.tables = tables()
-        
-        self.jobs = jobs()
-        self.config = config()
-        self.sessions = sessions()
-        self.execute = execute()
-        self.exceptions = exceptions()
-        self.local_storage = local_storage()
-        self.global_storage = global_storage()
-        
-        self.modules = modules()
-        self.plugins = plugins()
-        
-        self.adb_tools = adb_tools()
-        self.fsmanip = fsmanip()
-        self.hatvenom = hatvenom()
-        self.web_tools = web_tools()
-        self.tcp_tools = tcp_tools()
-        self.pseudo_shell = pseudo_shell()
-        self.string_tools = string_tools()
-
-        self.details = {
-            'Category': "",
-            'Name': "",
-            'Description': "",
-            'Usage': "",
-            'MinArgs': 0
-        }
+class HatSploitCommand(HatSploit):
+    HatSploit.details = {
+        'Category': "",
+        'Name': "",
+        'Description': "",
+        'Usage': "",
+        'MinArgs': 0
+    }
 
     def run(self, argc, argv):
         pass
