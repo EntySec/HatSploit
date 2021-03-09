2a8170739e7f44c7b22d758e6069095afa0abc81
Ivan Nikolsky <enty8080@gmail.com>
2021-03-09 22:09:07 +0300

    Update exit.py

--- core/commands/exit.py
+++ core/commands/exit.py
@@ -35,8 +35,8 @@ class HatSploitCommand(HatSploitCommand):
 
     usage = ""
     usage += "exit [option]\n\n"
-    usage += "    -h, --help   Show this help message.\n"
-    usage += "    -f, --force  Force exit, ignoring active jobs."
+    usage += "  -h, --help   Show this help message.\n"
+    usage += "  -f, --force  Force exit, ignoring active jobs."
 
     details = {
         'Category': "core",
