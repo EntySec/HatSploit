c760b6bde236534e365cc4d493a16d8ac71d3ee8
Ivan Nikolsky <enty8080@gmail.com>
2021-03-13 17:25:51 +0300

    Update shell_reverse_tcp.py

--- libs/payloads/linux/mipsle/shell_reverse_tcp.py
+++ libs/payloads/linux/mipsle/shell_reverse_tcp.py
@@ -137,4 +137,4 @@ class HatSploitPayload(HatSploitPayload):
 
         self.payload = payload
         self.instructions = instructions
-        self.actions = 'reverse_tcp'
+        self.action = 'reverse_tcp'
