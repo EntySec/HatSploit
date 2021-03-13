9efb61b90be049f393940c1cf7e6a1fe3b7ab1c1
Ivan Nikolsky <enty8080@gmail.com>
2021-03-13 22:50:02 +0300

    Update db.py

--- core/db/db.py
+++ core/db/db.py
@@ -42,6 +42,14 @@ class db:
                 return "plugins"
         return "modules"
         
+    def disconnect_payloads_database(self, name):
+        if self.local_storage.get("connected_payloads_databases"):
+            if name in self.local_storage.get("connected_payloads_databases"):
+                self.local_storage.delete_element("connected_modules_databases", name)
+                self.local_storage.delete_element("payloads", name)
+                return
+        self.badges.output_error("No such payloads database connected!")
+        
     def disconnect_modules_database(self, name):
         if self.local_storage.get("connected_modules_databases"):
             if name in self.local_storage.get("connected_modules_databases"):
@@ -57,6 +65,47 @@ class db:
                 self.local_storage.delete_element("plugins", name)
                 return
         self.badges.output_error("No such plugins database connected!")
+        
+    def connect_payloads_database(self, name, path):
+        if self.local_storage.get("connected_payloads_databases"):
+            if name in self.local_storage.get("connected_payloads_databases"):
+                self.bagdes.output_error("Payloads database already connected!")
+                return
+        if not os.path.exists(path) or not str.endswith(path, "json"):
+            self.badges.output_error("Not a payloads database!")
+            return
+        
+        try:
+            database = json.load(open(path))
+        except Exception:
+            self.badges.output_error("Failed to connect payloads database!")
+            return
+        
+        if '__database__' not in database.keys():
+            self.badges.output_error("No __database__ section found!")
+            return
+        if database['__database__']['type'] != "payloads":
+            self.badges.output_error("Not a payloads database!")
+            return
+        del database['__database__']
+
+        payloads = {
+            name: database
+        }
+        
+        data = {
+            name: {
+                'path': path
+            }
+        }
+        if not self.local_storage.get("connected_payloads_databases"):
+            self.local_storage.set("connected_payloads_databases", dict())
+        self.local_storage.update("connected_payloads_databases", data)
+        
+        if self.local_storage.get("payloads"):
+            self.local_storage.update("payloads", payloads)
+        else:
+            self.local_storage.set("payloads", payloads)
         
     def connect_modules_database(self, name, path):
         if self.local_storage.get("connected_modules_databases"):
