2a5e3a641a7a5ac97567128b21b597615fce71e9
Ivan Nikolsky <enty8080@gmail.com>
2021-03-13 17:22:31 +0300

    Update shell_bind_tcp.py

--- libs/payloads/linux/armle/shell_bind_tcp.py
+++ libs/payloads/linux/armle/shell_bind_tcp.py
@@ -55,7 +55,7 @@ class HatSploitPayload(HatSploitPayload):
         }
     }
 
-    def generate(self):
+    def run(self):
         bind_port, executable_format = self.parser.parse_options(self.options)
         bind_port = self.payload_generator.port_to_bytes(bind_port)
 
