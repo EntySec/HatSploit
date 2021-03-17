5d2331916ba1c00847657eda1d2a5761054cdee0
Ivan Nikolsky <enty8080@gmail.com>
2021-03-17 22:10:25 +0300

    Update shell_reverse_tcp.py

--- payloads/linux/mipsbe/shell_reverse_tcp.py
+++ payloads/linux/mipsbe/shell_reverse_tcp.py
@@ -34,7 +34,6 @@ class HatSploitPayload(HatSploitPayload):
     payload_generator = payload_generator()
 
     details = {
-        'Category': "linux/shell",
         'Name': "Linux mipsbe Shell Reverse TCP",
         'Payload': "linux/mipsbe/shell_reverse_tcp",
         'Authors': [
@@ -47,6 +46,7 @@ class HatSploitPayload(HatSploitPayload):
         'Comments': [
             ''
         ],
+        'Platform': "linux",
         'Risk': "high",
         'Type': "reverse_tcp"
     }
