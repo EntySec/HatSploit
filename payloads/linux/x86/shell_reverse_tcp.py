3028b7845621eba8dd8c5d888320fd23b387b4fb
Ivan Nikolsky <enty8080@gmail.com>
2021-03-17 22:12:47 +0300

    Update shell_reverse_tcp.py

--- payloads/linux/x86/shell_reverse_tcp.py
+++ payloads/linux/x86/shell_reverse_tcp.py
@@ -34,7 +34,6 @@ class HatSploitPayload(HatSploitPayload):
     payload_generator = payload_generator()
 
     details = {
-        'Category': "linux/shell",
         'Name': "Linux x86 Shell Reverse TCP",
         'Payload': "linux/x86/shell_reverse_tcp",
         'Authors': [
@@ -47,6 +46,7 @@ class HatSploitPayload(HatSploitPayload):
         'Comments': [
             ''
         ],
+        'Platform': "linux",
         'Risk': "high",
         'Type': "reverse_tcp"
     }
