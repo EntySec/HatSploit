d96873fc4d4cd48dfd1cb30b4cee0fb5d6d970e4
Ivan Nikolsky <enty8080@gmail.com>
2021-03-17 22:03:32 +0300

    Update shell_reverse_tcp.py

--- payloads/macos/x64/shell_reverse_tcp.py
+++ payloads/macos/x64/shell_reverse_tcp.py
@@ -47,6 +47,7 @@ class HatSploitPayload(HatSploitPayload):
         'Comments': [
             ''
         ],
+        'Platform': "macos",
         'Risk': "high",
         'Type': "reverse_tcp"
     }
