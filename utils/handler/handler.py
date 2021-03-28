5afdec83f81932080d57d15f017c3a07ef575d71
Ivan Nikolsky <enty8080@gmail.com>
2021-03-28 14:12:08 +0300

    Update handler.py

--- utils/handler/handler.py
+++ utils/handler/handler.py
@@ -129,7 +129,7 @@ class Handler(TCP):
                         return False
 
                 if payload['Category'].lower() == 'stager':
-                    self.echo_stage(payload['Payload'], new_session.send_command, payload['Args'], location)
+                    self.echo_stage(payload['Payload'], new_session.send_command, payload['Args'], location, encode=True)
                 elif payload['Category'].lower() == 'single':
                     new_session.send_command(payload['Payload'])
                 else:
