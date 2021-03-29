7f6c2307f78e15cd23aa342acff1825161cd4cdd
Ivan Nikolsky <enty8080@gmail.com>
2021-03-29 18:28:11 +0300

    Update search.py

--- core/commands/search.py
+++ core/commands/search.py
@@ -42,5 +42,61 @@ class HatSploitCommand(Command):
         'MinArgs': 1
     }
 
+    def show_plugins(self, keyword):
+        plugins = self.local_storage.get("plugins")
+        headers = ("Number", "Name", "Description")
+        for database in plugins.keys():
+            number = 0
+            plugins_data = list()
+            plugins = plugins[database]
+            for plugin in sorted(plugins.keys()):
+                if keyword in plugin:
+                    name = plugin.replace(keyword, self.RED + keyword + self.END)
+                    plugins_data.append((number, name, plugins[plugin]['Description']))
+                    number += 1
+            if plugins_data:
+                self.print_table("Plugins (" + database + ")", headers, *plugins_data)
+
+    def show_modules(self, keyword):
+        modules = self.local_storage.get("modules")
+        headers = ("Number", "Module", "Risk", "Description")
+        for database in modules.keys():
+            number = 0
+            modules_data = list()
+            for information in modules[database].keys():
+                for platform in sorted(modules[database][information].keys()):
+                    for module in sorted(modules[database][infotmation][platform].keys()):
+                        if keyword in information + '/' + platform + '/' + module:
+                            current_module = modules[database][information][platform]
+                            name = current_module[module]['Module'].replace(keyword, self.RED + keyword + self.END)
+                            modules_data.append((number, name, current_module[module]['Risk'],
+                                                current_module[module]['Description']))
+                            number += 1
+            if modules_data:
+                self.print_table(" Modules (" + database + ")", headers, *modules_data)
+
+    def show_payloads(self, keyword):
+        payloads = self.local_storage.get("payloads")
+        headers = ("Number", "Category", "Payload", "Risk", "Description")
+
+        for database in sorted(payloads.keys()):
+            number = 0
+            payloads_data = list()
+            for platform in sorted(payloads[database].keys()):
+                for architecture in sorted(payloads[database][platform].keys()):
+                    for payload in sorted(payloads[database][platform][architecture].keys()):
+                        if keyword in platform + '/' + architecture + '/' + payload:
+                            current_payload = payloads[database][platform][architecture][payload]
+                            name = current_payload['Payload'].replace(keyword, self.RED + keyword + self.END)
+                            payloads_data.append((number, current_payload['Category'], current_payload['Payload'],
+                                                current_payload['Risk'], current_payload['Description']))
+                            number += 1
+            if payloads_data:
+                self.print_table("Payloads (" + database + ")", headers, *payloads_data)
+
     def run(self, argc, argv):
-        pass #trough
+        keyword = argv[0]
+
+        self.show_payloads(keyword)
+        self.show_modules(keyword)
+        self.show_plugins(keyword)
