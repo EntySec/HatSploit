552f454c69d407f8d5bb3e9be213289a9400be4f
Ivan Nikolsky <enty8080@gmail.com>
2021-06-05 20:01:05 +0300

    Update tcp.py

--- utils/tcp/tcp.py
+++ utils/tcp/tcp.py
@@ -24,23 +24,43 @@
 # SOFTWARE.
 #
 
-import re
-import selectors
 import socket
-import sys
-import telnetlib
-import time
 
-from core.base.exceptions import Exceptions
 from core.cli.badges import Badges
 
 
+class TCPSocket:
+    def __init__(self, host, port, timeout=10):
+        self.host = host
+        self.port = int(port)
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(timeout)
+
+        self.badges = Badges()
+
+    def connect(self, timeout=10):
+        try:
+            self.sock.connect((self.host, self.port))
+        except Exception:
+            self.badges.output_error("Failed to connect!")
+
+    def send(self, data):
+        try:
+            self.pipe.send(data)
+        except Exception:
+            self.badges.output_error("Failed to send data!")
+
+    def recv(self, size):
+        response = b""
+        try:
+            response = self.pipe.recv(size)
+        except Exception:
+            self.badges.output_error("Failed to send data!")
+        return response
+
+
 class TCPClient:
-    badges = Badges()
-    exceptions = Exceptions()
-
-    client = None
-
     @staticmethod
     def get_local_host():
         try:
@@ -54,97 +74,12 @@ class TCPClient:
         return local_host
 
     @staticmethod
-    def tcp_request(host, port, data, buffer_size=1024, timeout=10):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(timeout)
-        sock.connect((host, int(port)))
-        sock.send(data.encode())
-        output = sock.recv(buffer_size)
-        sock.close()
-        return output.decode().strip()
-
-    @staticmethod
     def check_tcp_port(host, port, timeout=0.5):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.settimeout(timeout)
             if sock.connect_ex((host, int(port))) == 0:
                 return True
         return False
-    
+
     def open(self, host, port, timeout=10):
-        sock = socket.socket()
-        sock.settimeout(timeout)
-        sock.connect((host, int(port)))
-        return sock
-
-    def connect(self, client):
-        self.client = telnetlib.Telnet()
-        self.client.sock = client
-
-    def disconnect(self):
-        self.client.close()
-
-    def send(self, buffer):
-        if self.client:
-            self.client.write(buffer)
-
-    def interactive(self, terminator='\n'):
-        if self.client:
-            selector = selectors.SelectSelector()
-
-            selector.register(self.client, selectors.EVENT_READ)
-            selector.register(sys.stdin, selectors.EVENT_READ)
-
-            while True:
-                for key, events in selector.select():
-                    if key.fileobj is self.client:
-                        try:
-                            response = self.client.read_eager()
-                        except Exception:
-                            self.badges.output_warning("Connection terminated.")
-                            return
-                        if response:
-                            self.badges.output_empty(response.decode(), start='', end='')
-                    elif key.fileobj is sys.stdin:
-                        line = sys.stdin.readline().strip()
-                        if not line:
-                            pass
-                        if line == "exit":
-                            return
-                        self.client.write((line + terminator).encode())
-
-    def recv(self, timeout=10):
-        if self.client:
-            result = b""
-            if timeout is not None:
-                timeout = time.time() + timeout
-                while True:
-                    data = self.client.read_very_eager()
-                    result += data
-                    if data:
-                        break
-                    if time.time() > timeout:
-                        raise socket.timeout
-            else:
-                while True:
-                    data = self.client.read_very_eager()
-                    result += data
-                    if data:
-                        break
-            return result
-        return None
-
-    def send_cmd(self, command, output=True, timeout=10):
-        if self.client:
-            buffer = command.encode()
-            self.send(buffer)
-
-            if output:
-                try:
-                    output = self.recv(timeout)
-                    output = output.decode().strip()
-
-                    return output
-                except socket.timeout:
-                    self.badges.output_warning("Timeout waiting for response.")
-        return None
+        return TCPSocket(host, port, timeout)
