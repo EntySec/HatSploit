87e7c48ae112f5bda48b8b9f9714c34aaec5ee42
Ivan Nikolsky <enty8080@gmail.com>
2021-06-22 19:20:13 +0200

    Update check.py

--- hatsploit/core/utils/check.py
+++ hatsploit/core/utils/check.py
@@ -31,85 +31,86 @@ from hatsploit.core.base.config import Config
 from hatsploit.core.db.importer import Importer
 from hatsploit.core.cli.badges import Badges
 
-importer = Importer()
-badges = Badges()
 
-def check_modules():
-    one_fail = False
-    badges.output_process("Checking all stdalone modules...")
+class Check:
+    def __init__(self):
+        self.config = Config()
+        self.importer = Importer()
+        self.badges = Badges()
 
-    modules_path = 'modules'
-    for dest, _, files in os.walk(modules_path):
-        for file in files:
-            if file.endswith('.py') and file != '__init__.py':
-                module = dest + '/' + file[:-3]
-                module_name = module[len(f"{modules_path}/"):]
+    def check_modules(self):
+        one_fail = False
+        self.badges.output_process("Checking all stdalone modules...")
 
-                try:
-                    module_object = importer.import_module(module)
-                    keys = ['Name', 'Module', 'Authors', 'Description', 'Comments', 'Platform', 'Risk']
-                    assert(all(key in module_object.details for key in keys))
-                    badges.output_success(f"{module_name}: OK")
-                except Exception:
-                    badges.output_error(f"{module_name}: FAIL")
-                    one_fail = True
-    return one_fail
+        modules_path = 'modules'
+        for dest, _, files in os.walk(modules_path):
+            for file in files:
+                if file.endswith('.py') and file != '__init__.py':
+                    module = dest + '/' + file[:-3]
+                    module_name = module[len(f"{modules_path}/"):]
 
-def check_payloads():
-    one_fail = False
-    badges.output_process("Checking all stdalone payloads...")
+                    try:
+                        module_object = self.importer.import_module(module)
+                        keys = ['Name', 'Module', 'Authors', 'Description', 'Comments', 'Platform', 'Risk']
+                        assert(all(key in module_object.details for key in keys))
+                        self.badges.output_success(f"{module_name}: OK")
+                    except Exception:
+                        self.badges.output_error(f"{module_name}: FAIL")
+                        one_fail = True
+        return one_fail
 
-    payloads_path = 'payloads'
-    for dest, _, files in os.walk(payloads_path):
-        for file in files:
-            if file.endswith('.py') and file != '__init__.py':
-                payload = dest + '/' + file[:-3]
-                payload_name = payload[len(f"{payloads_path}/"):]
+    def check_payloads(self):
+        one_fail = False
+        self.badges.output_process("Checking all stdalone payloads...")
 
-                try:
-                    payload_object = importer.import_payload(payload)
-                    keys = ['Category', 'Name', 'Payload', 'Authors', 'Description',
-                            'Comments', 'Architecture', 'Platform', 'Risk', 'Type']
-                    assert(all(key in payload_object.details for key in keys))
-                    badges.output_success(f"{payload_name}: OK")
-                except Exception:
-                    badges.output_error(f"{payload_name}: FAIL")
-                    one_fail = True
-    return one_fail
+        payloads_path = 'payloads'
+        for dest, _, files in os.walk(payloads_path):
+            for file in files:
+                if file.endswith('.py') and file != '__init__.py':
+                    payload = dest + '/' + file[:-3]
+                    payload_name = payload[len(f"{payloads_path}/"):]
 
-def check_plugins():
-    one_fail = False
-    badges.output_process("Checking all stdalone plugins...")
+                    try:
+                        payload_object = self.importer.import_payload(payload)
+                        keys = ['Category', 'Name', 'Payload', 'Authors', 'Description',
+                                'Comments', 'Architecture', 'Platform', 'Risk', 'Type']
+                        assert(all(key in payload_object.details for key in keys))
+                        self.badges.output_success(f"{payload_name}: OK")
+                    except Exception:
+                        self.badges.output_error(f"{payload_name}: FAIL")
+                        one_fail = True
+        return one_fail
 
-    plugins_path = 'plugins'
-    for dest, _, files in os.walk(plugins_path):
-        for file in files:
-            if file.endswith('.py') and file != '__init__.py':
-                plugin = dest + '/' + file[:-3]
-                plugin_name = plugin[len(f"{plugins_path}/"):]
+    def check_plugins(self):
+        one_fail = False
+        self.badges.output_process("Checking all stdalone plugins...")
 
-                try:
-                    plugin_object = importer.import_plugin(plugin)
-                    keys = ['Name', 'Authors', 'Description', 'Comments']
-                    assert(all(key in plugin_object.details for key in keys))
-                    badges.output_success(f"{plugin_name}: OK")
-                except Exception:
-                    badges.output_error(f"{plugin_name}: FAIL")
-                    one_fail = True
-    return one_fail
+        plugins_path = 'plugins'
+        for dest, _, files in os.walk(plugins_path):
+            for file in files:
+                if file.endswith('.py') and file != '__init__.py':
+                    plugin = dest + '/' + file[:-3]
+                    plugin_name = plugin[len(f"{plugins_path}/"):]
 
-def check_all():
-    fails = list()
+                    try:
+                        plugin_object = self.importer.import_plugin(plugin)
+                        keys = ['Name', 'Authors', 'Description', 'Comments']
+                        assert(all(key in plugin_object.details for key in keys))
+                        self.badges.output_success(f"{plugin_name}: OK")
+                    except Exception:
+                        self.badges.output_error(f"{plugin_name}: FAIL")
+                        one_fail = True
+        return one_fail
 
-    fails.append(check_modules())
-    fails.append(check_payloads())
-    fails.append(check_plugins())
+    def check_all(self):
+        fails = list()
+
+        fails.append(check_modules())
+        fails.append(check_payloads())
+        fails.append(check_plugins())
     
-    for fail in fails:
-        if fail:
-            badges.output_error("Not all checks passed!")
-            sys.exit(1)
-    badges.output_success("All checks passed!")
-
-if __name__ == '__main__':
-    check_all()
+        for fail in fails:
+            if fail:
+                self.badges.output_error("Not all checks passed!")
+                return
+        self.badges.output_success("All checks passed!")
