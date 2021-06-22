bdb4342d38a4393ff93aa3be20acbc171b6a1c72
Ivan Nikolsky <enty8080@gmail.com>
2021-06-23 01:46:00 +0200

    Update membrane_bind_tcp.py

--- payloads/macos/x64/membrane_bind_tcp.py
+++ payloads/macos/x64/membrane_bind_tcp.py
@@ -68,7 +68,7 @@ class HatSploitPayload(Payload, StringTools):
         bind_port = self.xor_string(bind_port)
 
         self.output_process("Generating payload...")
-        with open('data/membrane/macos/x64/membrane', 'rb') as f:
+        with open(self.data_path + 'membrane/macos/x64/membrane', 'rb') as f:
             payload = f.read()
 
         return payload, f"bind '{bind_port}'", HatSploitSession
