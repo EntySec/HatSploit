dca6418232e2bf5e2ab2caff9031a62d8bf7df7b
Ivan Nikolsky <enty8080@gmail.com>
2021-06-24 14:54:15 +0200

    Update hsf.py

--- hatsploit/hsf.py
+++ hatsploit/hsf.py
@@ -64,7 +64,7 @@ Using this functional against target systems without prior permission is illegal
 The authors are not liable for any damages from misuse of this information or code.
 
 If you are planning on using HatSploit Framework for malicious purposes that are not authorized by the company 
-you are performing assessments for, you are violating the terms of service and license of this Framework. 
+you are performing assessments for, you are violating the terms of service and license of this tool. 
 
 By accepting our terms of service, you agree that you will only use this tool for lawful purposes only.
 """
