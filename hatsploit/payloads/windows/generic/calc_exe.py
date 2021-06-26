7a14e8bb7fe3de49131459e0942ecf9085c568e3
Ivan Nikolsky <enty8080@gmail.com>
2021-06-26 21:13:23 +0200

    Create calc_exe.py

---
+++ hatsploit/payloads/windows/generic/calc_exe.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.payload import Payload
+from hatsploit.utils.tcp import TCPClient
+
+
+class HatSploitPayload(Payload, TCPClient):
+    details = {
+        'Category': "single",
+        'Name': "Windows Calculator",
+        'Payload': "windows/generic/calc_exe",
+        'Authors': [
+            'Ivan Nikolsky (enty8080)'
+        ],
+        'Description': "Windows calc.exe payload.",
+        'Comments': [
+            ''
+        ],
+        'Architecture': "generic",
+        'Platform': "windows",
+        'Risk': "high",
+        'Type': "one_side"
+    }
+
+    options = {
+        'LHOST': {
+            'Description': "Local host.",
+            'Value': TCPClient.get_local_host(),
+            'Type': "ip",
+            'Required': True
+        },
+        'LPORT': {
+            'Description': "Local port.",
+            'Value': 8888,
+            'Type': "port",
+            'Required': True
+        }
+    }
+
+    def run(self):
+        local_host, local_port = self.parse_options(self.options)
+
+        self.output_process("Generating payload...")
+        payload = "C:\Windows\System32\calc.exe"
+
+        return payload
