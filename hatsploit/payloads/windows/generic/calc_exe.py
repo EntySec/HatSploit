69b80a2960076264484c4dc302c9da6ea8be0573
Ivan Nikolsky <enty8080@gmail.com>
2021-06-27 08:55:55 +0200

    Create calc_exe.py

---
+++ hatsploit/payloads/windows/generic/calc_exe.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.payload import Payload
+
+
+class HatSploitPayload(Payload):
+    details = {
+        'Category': "single",
+        'Name': "Windows Calculator",
+        'Payload': "windows/generic/calc_exe",
+        'Authors': [
+            'Ivan Nikolsky (enty8080)'
+        ],
+        'Description': "Windows calc.exe payload.",
+        'Comments': [
+            ''
+        ],
+        'Architecture': "generic",
+        'Platform': "windows",
+        'Risk': "high",
+        'Type': "one_side"
+    }
+
+    def run(self):
+        self.output_process("Generating payload...")
+        payload = "C:\Windows\System32\calc.exe"
+
+        return payload
