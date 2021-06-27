a5b122b5b9c99cd519952c1929cd802f7febd50a
Ivan Nikolsky <enty8080@gmail.com>
2021-06-27 14:57:52 +0200

    Create message_box.py

---
+++ hatsploit/payloads/windows/generic/message_box.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+import struct
+
+from hatvenom import HatVenom
+from hatsploit.lib.payload import Payload
+
+
+class HatSploitPayload(Payload, HatVenom):
+    details = {
+        'Category': "single",
+        'Name': "Windows Message Box",
+        'Payload': "windows/generic/message_box",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - payload developer'
+        ],
+        'Description': "Message Box payload for Windows.",
+        'Comments': [
+            ''
+        ],
+        'Architecture': "generic",
+        'Platform': "windows",
+        'Risk': "low",
+        'Type': "one_side"
+    }
+
+    options = {
+        'MESSAGE': {
+            'Description': "Message to show.",
+            'Value': "Hello, Friend!",
+            'Type': None,
+            'Required': True
+        }
+    }
+
+    def run(self):
+        message = self.parse_options(self.options)
+
+        source = (
+            "[reflection.assembly]::loadwithpartialname('system.windows.forms');"
+            f"[system.Windows.Forms.MessageBox]::show('{message}')"
+        )
+
+        payload = f"powershell -w hidden -nop -c {source}"
+        return payload
