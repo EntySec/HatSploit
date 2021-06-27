d11d4b3fa79093a5b0e45bef1b7b8ac90d97bc97
Ivan Nikolsky <enty8080@gmail.com>
2021-06-27 13:01:23 +0200

    Create say.py

---
+++ hatsploit/payloads/windows/generic/say.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+import struct
+
+from hatvenom import HatVenom
+from hatsploit.lib.payload import Payload
+
+
+class HatSploitPayload(Payload, HatVenom):
+    details = {
+        'Category': "stager",
+        'Name': "Windows Say",
+        'Payload': "windows/generic/say",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - payload developer'
+        ],
+        'Description': "Say payload for Windows.",
+        'Comments': [
+            ''
+        ],
+        'Architecture': "generic",
+        'Platform': "windows",
+        'Risk': "low",
+        'Type': "one_side"
+    }
+
+    options = {
+        'MESSAGE': {
+            'Description': "Message to say.",
+            'Value': "Hello, Friend!",
+            'Type': None,
+            'Required': True
+        }
+    }
+
+    def run(self):
+        message = self.parse_options(self.options)
+
+        self.output_process("Generating payload...")
+        payload = """
+Function Invoke-VoiceTroll
+{
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $True, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [String] $VoiceText
+    )
+    Set-StrictMode -version 2
+    Add-Type -AssemblyName System.Speech
+    $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer
+    $synth.Speak($VoiceText)
+}
+Invoke-VoiceTroll"""
+
+        return payload
