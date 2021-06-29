8540a4a84e568cfc206d1dca82f19a528d84fd4d
Ivan Nikolsky <enty8080@gmail.com>
2021-06-29 16:28:42 +0300

    Update handler.py

--- hatsploit/utils/handler.py
+++ hatsploit/utils/handler.py
@@ -28,8 +28,6 @@ import os
 import requests
 import binascii
 
-import threading
-
 from hatsploit.lib.sessions import Sessions
 from hatsploit.lib.storage import LocalStorage
 from hatsploit.core.cli.badges import Badges
@@ -349,19 +347,11 @@ class Handler(Server):
                             ]
                         )
                 elif payload['Category'].lower() == 'single':
-                    t = threading.Thread(
-                        target=new_session.send_command,
-                        args=[payload['Payload']]
-                    )
-                    t.setDaemon(True)
-                    t.start()
-                    '''
                     self.do_job(
                         "Handler Stage",
                         new_session.send_command,
                         [payload['Payload']]
                     )
-                    '''
                 else:
                     self.badges.output_error("Invalid payload category!")
                     return False
