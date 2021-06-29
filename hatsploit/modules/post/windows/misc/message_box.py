4c5654b2450fe00a537bb7af498725c17afe9a21
Ivan Nikolsky <enty8080@gmail.com>
2021-06-29 18:19:16 +0300

    Create message_box.py

---
+++ hatsploit/modules/post/windows/misc/message_box.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+#
+# This module requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.module import Module
+from hatsploit.utils.session import SessionTools
+
+
+class HatSploitModule(Module, SessionTools):
+    details = {
+        'Name': "Windows Invoke Message Box",
+        'Module': "post/windows/misc/message_box",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - module developer'
+        ],
+        'Description': "Invoke message box on Windows machine.",
+        'Comments': [
+            ''
+        ],
+        'Platform': "windows",
+        'Risk': "medium"
+    }
+
+    options = {
+        'SESSION': {
+            'Description': "Session to run on.",
+            'Value': 0,
+            'Type': "session",
+            'Required': True
+        },
+        'MESSAGE': {
+            'Description': "Message to show.",
+            'Value': "Hello, Friend!",
+            'Type': None,
+            'Required': True
+        }
+    }
+
+    def run(self):
+        session, message = self.parse_options(self.options)
+
+        source = (
+            "[reflection.assembly]::loadwithpartialname('system.windows.forms');"
+            f"[system.Windows.Forms.MessageBox]::show('{message}')"
+        )
+
+        session = self.get_session('windows', 'shell', session)
+        session.send_command(f"powershell -w hidden -nop -c {source}")
