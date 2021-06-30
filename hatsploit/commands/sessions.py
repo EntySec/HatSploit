827b12fea629525d7f8edf64596828fbf4fe4389
Ivan Nikolsky <enty8080@gmail.com>
2021-06-30 22:09:44 +0300

    Create sessions.py

--- hatsploit/commands/sessions.py
+++ hatsploit/commands/sessions.py
@@ -16,10 +16,9 @@ class HatSploitCommand(Command):
 
     usage = ""
     usage += "sessions <option> [arguments]\n\n"
-    usage += "  -l, --list [session_platform]                   List all opened sessions\n"
-    usage += "                                                  [for specified session platform].\n"
-    usage += "  -i, --interact <session_platform> <session_id>  Interact with specified session.\n"
-    usage += "  -c, --close <session_platform> <session_id>     Close specified session.\n"
+    usage += "  -l, --list                   List all opened sessions\n"
+    usage += "  -i, --interact <session_id>  Interact with specified session.\n"
+    usage += "  -c, --close <session_id>     Close specified session.\n"
 
     details = {
         'Category': "sessions",
@@ -35,43 +34,28 @@ class HatSploitCommand(Command):
     def run(self, argc, argv):
         if argv[0] in ['-l', '--list']:
             sessions = self.local_storage.get("sessions")
-            if argc < 2:
-                if sessions:
-                    for session_platform in sessions.keys():
-                        sessions_data = list()
-                        headers = ("ID", "Type", "Host", "Port")
-                        for session_id in sessions[session_platform].keys():
-                            session_type = sessions[session_platform][session_id]['type']
-                            host = sessions[session_platform][session_id]['host']
-                            port = sessions[session_platform][session_id]['port']
+            if sessions:
+                sessions_data = list()
+                headers = ("ID", "Platform", "Type", "Host", "Port")
+                for session_id in sessions.keys():
+                    session_platform = sessions[session_id]['platform']
+                    session_type = sessions[session_id]['type']
+                    host = sessions[session_id]['host']
+                    port = sessions[session_id]['port']
 
-                            sessions_data.append((session_id, session_type, host, port))
-                        self.print_table("Opened Sessions (" + session_platform + ")", headers, *sessions_data)
-                else:
-                    self.output_warning("No opened sessions available.")
+                    sessions_data.append((session_id, session_platform, session_type, host, port))
+                self.print_table("Opened Sessions", headers, *sessions_data)
             else:
-                if argv[1] in sessions.keys():
-                    session_platform = argv[1]
-                    sessions_data = list()
-                    headers = ("ID", "Type", "Host", "Port")
-                    for session_id in sessions[session_platform].keys():
-                        session_type = sessions[session_platform][session_id]['type']
-                        host = sessions[session_platform][session_id]['host']
-                        port = sessions[session_platform][session_id]['port']
-
-                        sessions_data.append((session_id, session_type, host, port))
-                    self.print_table("Opened Sessions (" + session_platform + ")", headers, *sessions_data)
-                else:
-                    self.output_error("Invalid session platform given!")
+                self.output_warning("No opened sessions available.")
         elif argv[0] in ['-c', '--close']:
             if argc < 3:
                 self.output_usage(self.details['Usage'])
             else:
-                self.sessions.close_session(argv[1], argv[2])
+                self.sessions.close_session(argv[1])
         elif argv[0] in ['-i', '--interact']:
             if argc < 3:
                 self.output_usage(self.details['Usage'])
             else:
-                self.sessions.spawn_interactive_connection(argv[1], argv[2])
+                self.sessions.spawn_interactive_connection(argv[1])
         else:
             self.output_usage(self.details['Usage'])
