cdddd4c82d9741645e5edaa7536a9a0c92817bbf
Ivan Nikolsky <enty8080@gmail.com>
2021-06-30 14:24:34 +0300

    Create api.py

---
+++ hatsploit/core/utils/api.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+#
+# MIT License
+#
+# Copyright (c) 2020-2021 EntySec
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import ast
+
+from hatsploit.lib.sessions import Sessions
+
+class SessionManager(Resource):
+    sessions = Sessions()
+
+    def get(self):
+        # GET /sessions
+        sessions = self.sessions.get_all_sessions()
+        if sessions:
+            return {'data': sessions}, 200
+        return {'data': dict()}, 200
+
+    def post(self):
+        # POST /sessions?platform=windows?type=shell?id=0?command=calc.exe
+        parser = reqparse.RequestParser()
+        parser.add_argument('platform', required=True)
+        parser.add_argument('type', required=True)
+        parser.add_argument('id', required=True)
+        parser.add_argument('command', required=True)
+        args = parser.parse_args()
+
+        session = self.sessions.get_session(
+            args['platform'], args['type'], args['id']
+        )
+        
+        if session:
+            return session.send_command(args['command'], output=True), 200
+        return "", 404
+
+class API:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app)
+
+    def init(self):
+        self.api.add_resource(SessionManager, '/sessions')
+        self.app.run()
+    
