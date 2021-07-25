9b7092d974224176ada903a2cd997437a45e4e3a
Ivan Nikolsky <enty8080@gmail.com>
2021-07-26 00:43:44 +0200

    Update hsf.py

--- hatsploit/hsf.py
+++ hatsploit/hsf.py
@@ -104,6 +104,7 @@ def main():
     parser.add_argument('--port', dest='port', type=int, help='HatSploit REST API port. [default: 8008]')
     parser.add_argument('-s', '--script', dest='script', help='Execute HatSploit commands from script file.')
     parser.add_argument('--no-exit', dest='no_exit', action='store_true', help='Do not exit after script execution.')
+    parser.add_argument('--no-startup', dest='no_startup', action='store_true', help='Do not execute startup.hsf file.')
     args = parser.parse_args()
 
     hsf = HatSploit()
@@ -136,14 +137,35 @@ def main():
                 "None",
                 hsf.api.init
             )
-        hsf.launch()
+        if args.no_startup():
+            hsf.launch()
+        else:
+            if os.path.exists(hsf.root_path + 'startup.hsf'):
+                hsf.launch(script=hsf.root_path + 'startup.hsf')
+            else:
+                hsf.launch()
     elif args.script:
         if not os.path.exists(args.script):
             hsf.badges.output_error(f"Local file: {args.script}: does not exist!")
             sys.exit(1)
-        hsf.launch(
-            do_shell=args.no_exit,
-            script=args.script
-        )
+        if args.no_startup():
+            hsf.launch(
+                do_shell=args.no_exit,
+                script=args.script
+            )
+        else:
+            if os.path.exists(hsf.root_path + 'startup.hsf'):
+                hsf.launch(script=hsf.root_path + 'startup.hsf')
+            else:
+                hsf.launch(
+                    do_shell=args.no_exit,
+                    script=args.script
+                )
     else:
-        hsf.launch()
+        if args.no_startup():
+            hsf.launch()
+        else:
+            if os.path.exists(hsf.root_path + 'startup.hsf'):
+                hsf.launch(script=hsf.root_path + 'startup.hsf')
+            else:
+                hsf.launch()
