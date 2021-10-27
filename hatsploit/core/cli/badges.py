9455a5d104d12e337010b4d0eb762f2e65e01307
Ivan Nikolsky <enty8080@gmail.com>
2021-10-27 23:37:48 +0300

    Update badges.py

--- hatsploit/core/cli/badges.py
+++ hatsploit/core/cli/badges.py
@@ -27,54 +27,50 @@
 import os
 
 from hatsploit.core.base.io import IO
-from hatsploit.core.cli.colors import Colors
+from hatsploit.core.utils.ui.colors_script import ColorsScript
 
 
 class Badges:
     io = IO()
-    colors = Colors()
-
-    I = colors.WHITE + colors.BOLD + '[i] ' + colors.END
-    S = colors.GREEN + colors.BOLD + '[+] ' + colors.END
-    W = colors.YELLOW + colors.BOLD + '[!] ' + colors.END
-    E = colors.RED + colors.BOLD + '[-] ' + colors.END
-    P = colors.BLUE + colors.BOLD + '[*] ' + colors.END
-    Q = colors.WHITE + colors.BOLD + '[?] ' + colors.END
-    A = colors.WHITE + colors.BOLD + '[>] ' + colors.END
+    colors_script = ColorsScript()
 
     def print_empty(self, message="", start='\033[1K\r', end='\n'):
-        self.io.print(message, start=start, end=end)
+        line = self.colors_script.parse(message)
+        self.io.print(line, start=start, end=end)
 
     def print_usage(self, message, start='\033[1K\r', end='\n'):
-        self.print_empty("Usage: " + message, start=start, end=end)
+        self.print_empty(f"Usage: {message}", start=start, end=end)
 
     def print_process(self, message, start='\033[1K\r', end='\n'):
-        self.print_empty(self.P + message, start=start, end=end)
+        self.print_empty(f"%bold%blue[*]%end {message}", start=start, end=end)
 
     def print_success(self, message, start='\033[1K\r', end='\n'):
-        self.print_empty(self.S + message, start=start, end=end)
+        self.print_empty(f"%bold%green[+]%end {message}", start=start, end=end)
 
     def print_error(self, message, start='\033[1K\r', end='\n'):
-        self.print_empty(self.E + message, start=start, end=end)
+        self.print_empty(f"%bold%red[-]%end {message}", start=start, end=end)
 
     def print_warning(self, message, start='\033[1K\r', end='\n'):
-        self.print_empty(self.W + message, start=start, end=end)
+        self.print_empty(f"%bold%yellow[!]%end {message}", start=start, end=end)
 
     def print_information(self, message, start='\033[1K\r', end='\n'):
-        self.print_empty(self.I + message, start=start, end=end)
+        self.print_empty(f"%bold%white[i]%end {message}", start=start, end=end)
 
     def print_multi(self, message):
-        self.print_empty(f"\r{self.P}{message}", end='')
+        self.print_empty(f"%bold%blue[*]%end {message}", start='\r', end='')
 
     def input_empty(self, message):
+        line = self.colors_script.parse(message)
+
         output = ""
-        out = self.io.input(message)[0]
+        out = self.io.input(line)[0]
+
         for command in out:
             output += command + " "
         return output.strip()
 
     def input_question(self, message):
-        return self.input_empty(self.Q + message)
+        return self.input_empty(f"%bold%white[?]%end {message}")
 
     def input_arrow(self, message):
-        return self.input_empty(self.A + message)
+        return self.input_empty(f"%bold%white[>]%end {message}")
