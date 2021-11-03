10571fb023093a0fb099af89b1c5303a6a7dc6ae
Ivan Nikolsky <enty8080@gmail.com>
2021-11-03 20:02:17 +0300

    Update use.py

--- hatsploit/commands/use.py
+++ hatsploit/commands/use.py
@@ -30,7 +30,7 @@ class HatSploitCommand(Command):
         return False
 
     def run(self, argc, argv):
-        module = argv[0]
+        module = argv[1]
 
         if not self.check_if_already_used(module):
             if self.modules.check_exist(module):
