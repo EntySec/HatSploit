678852fb9065ba52bf5f8b980c5874609cd94f15
Ivan Nikolsky <enty8080@gmail.com>
2021-11-03 16:49:11 +0300

    Update sessions.py

--- hatsploit/lib/sessions.py
+++ hatsploit/lib/sessions.py
@@ -42,7 +42,7 @@ class Sessions:
         if not self.local_storage.get("sessions"):
             return True
         self.badges.print_warning("You have some opened sessions.")
-        if self.badges.input_question("Exit anyway? [y/N] ").lower() in ['yes', 'y']:
+        if self.badges.input_question("Exit anyway? [y/N] ")[0].lower() in ['yes', 'y']:
             self.badges.print_process("Closing all sessions...")
             self.close_all_sessions()
             return True
