d4639ad6fabdf6c4af085e4442c07561268bb331
Ivan Nikolsky <enty8080@gmail.com>
2021-11-03 20:10:39 +0300

    Create getpid.py

---
+++ hatsploit/external/pwny/commands/getpid.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#
+# This command requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.command import Command
+
+
+class HatSploitCommand(Command):
+    details = {
+        'Category': "gather",
+        'Name': "getpid",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - command developer'
+        ],
+        'Description': "Get Pwny process id.",
+        'Usage': "getpid",
+        'MinArgs': 0
+    }
+
+    def run(self, argc, argv):
+        output = self.session.send_command(
+            ' '.join(argv)
+            output=True
+        )
+
+        if output:
+            self.print_empty(output)
