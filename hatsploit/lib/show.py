ca433069b90e7ee7f58b0317cf9d82dd746b08d3
Ivan Nikolsky <enty8080@gmail.com>
2021-11-08 23:20:30 +0300

    Update show.py

--- hatsploit/lib/show.py
+++ hatsploit/lib/show.py
@@ -24,16 +24,20 @@
 # SOFTWARE.
 #
 
+from hatsploit.lib.jobs import Jobs
 from hatsploit.lib.storage import LocalStorage
 from hatsploit.lib.modules import Modules
 
+from hatsploit.core.cli.badges import Badges
 from hatsploit.core.cli.tables import Tables
 
 
 class Show:
+    jobs = Jobs()
     local_storage = LocalStorage()
     modules = Modules()
 
+    badges = Badges()
     tables = Tables()
 
     def show_custom_commands(self, handler):
@@ -51,7 +55,10 @@ class Show:
             self.tables.print_table(label.title() + " Commands", headers, *commands_data[label])
 
     def show_interface_commands(self):
-        self.show_custom_commands(self.local_storage.get("commands"))
+        if self.local_storage.get("commands"):
+            self.show_custom_commands(self.local_storage.get("commands"))
+        else:
+            self.badges.print_warning("No commands available.")
 
     def show_plugin_commands(self):
         for plugin in self.local_storage.get("loaded_plugins").keys():
@@ -83,3 +90,53 @@ class Show:
             self.show_module_commands()
         if self.local_storage.get("loaded_plugins"):
             self.show_plugin_commands()
+
+    def show_jobs(self):
+        if self.local_storage.get("jobs"):
+            jobs_data = list()
+            headers = ("ID", "Name", "Module")
+            jobs = self.local_storage.get("jobs")
+            for job_id in jobs.keys():
+                jobs_data.append((job_id, jobs[job_id]['job_name'], jobs[job_id]['module_name']))
+            self.tables.print_table("Active Jobs", headers, *jobs_data)
+        else:
+            self.badges.print_warning("No running jobs available.")
+
+    def show_module_databases(self):
+        if self.local_storage.get("connected_modules_databases"):
+            databases_data = list()
+            number = 0
+            headers = ("Number", "Name", "Path")
+            databases = self.local_storage.get("connected_modules_databases")
+            for name in databases.keys():
+                databases_data.append((number, name, databases[name]['path']))
+                number += 1
+            self.tables.print_table("Connected Module Databases", headers, *databases_data)
+        else:
+            self.badges.print_warning("No module database connected.")
+
+    def show_payload_databases(self):
+        if self.local_storage.get("connected_payloads_databases"):
+            databases_data = list()
+            number = 0
+            headers = ("Number", "Name", "Path")
+            databases = self.local_storage.get("connected_payload_databases")
+            for name in databases.keys():
+                databases_data.append((number, name, databases[name]['path']))
+                number += 1
+            self.tables.print_table("Connected Payload Databases", headers, *databases_data)
+        else:
+            self.badges.print_warning("No payload database connected.")
+
+    def show_plugin_databases(self):
+        if self.local_storage.get("connected_plugins_databases"):
+            databases_data = list()
+            number = 0
+            headers = ("Number", "Name", "Path")
+            databases = self.local_storage.get("connected_plugins_databases")
+            for name in databases.keys():
+                databases_data.append((number, name, databases[name]['path']))
+                number += 1
+            self.tables.print_table("Connected Plugin Databases", headers, *databases_data)
+        else:
+            self.badges.print_warning("No plugin database connected.")
