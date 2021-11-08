7149b566642f317ce41727844b6ce06c529bb3c7
Ivan Nikolsky <enty8080@gmail.com>
2021-11-09 00:12:27 +0300

    Update show.py

--- hatsploit/commands/show.py
+++ hatsploit/commands/show.py
@@ -14,6 +14,35 @@ class HatSploitCommand(Command):
     local_storage = LocalStorage()
     show = Show()
 
+    options = self.modules.check_current_module()
+    payloads = self.local_storage.get("payloads")
+    modules = self.local_storage.get("modules")
+    plugins = self.local_storage.get("plugins")
+
+    informations = list()
+    if modules:
+        for database in sorted(modules.keys()):
+            for module in sorted(modules[database].keys()):
+                info = module.split('/')[0]
+                if info not in informations:
+                    informations.append(info)
+
+    if informations or plugins or options:
+        usage = "show ["
+
+        if payloads:
+            usage += "payloads|"
+        for information in informations:
+            usage += information + "|"
+        if plugins:
+            usage += "plugins|"
+        if options:
+            usage += "options"
+        else:
+            usage = usage[:-2] + "]"
+    else:
+        usage = "show []"
+
     details = {
         'Category': "core",
         'Name': "show",
@@ -21,70 +50,24 @@ class HatSploitCommand(Command):
             'Ivan Nikolsky (enty8080) - command developer'
         ],
         'Description': "Show specified information.",
-        'Usage': self.usage(),
+        'Usage': usage,
         'MinArgs': 1
     }
 
-    def print_usage(self):
-        options = self.modules.check_current_module()
-        payloads = self.local_storage.get("payloads")
-        modules = self.local_storage.get("modules")
-        plugins = self.local_storage.get("plugins")
-
-        informations = list()
-        if modules:
-            for database in sorted(modules.keys()):
-                for module in sorted(modules[database].keys()):
-                    info = module.split('/')[0]
-                    if info not in informations:
-                        informations.append(info)
-
-        if informations or plugins or options:
-            usage = "show ["
-
-            if payloads:
-                usage += "payloads|"
-            for information in informations:
-                usage += information + "|"
-            if plugins:
-                usage += "plugins|"
-            if options:
-                usage += "options"
-            else:
-                usage = usage[:-2] + "]"
-            return usage
-        else:
-            return "show []"
-
     def run(self, argc, argv):
-        information = argv[1]
-
-        options = self.modules.check_current_module()
-        payloads = self.local_storage.get("payloads")
-        modules = self.local_storage.get("modules")
-        plugins = self.local_storage.get("plugins")
-
-        informations = list()
-        if modules:
-            for database in sorted(modules.keys()):
-                for module in sorted(modules[database].keys()):
-                    info = module.split('/')[0]
-                    if info not in informations:
-                        informations.append(info)
-
-        if payloads:
+        if self.payloads:
             if information == "payloads":
                 self.show.show_payloads()
                 return
-        if plugins:
+        if self.plugins:
             if information == "plugins":
                 self.show.show_plugins()
                 return
-        if options:
+        if self.options:
             if information == "options":
                 self.show.show_options()
                 return
-        if information in informations:
+        if information in self.informations:
             self.show.show_modules(information)
         else:
             self.print_usage(self.details['Usage'])
