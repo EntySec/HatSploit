1068388bae6d320fa59bd9aab8390bbfcdc92d1f
Ivan Nikolsky <enty8080@gmail.com>
2021-12-03 22:18:58 +0300

    Update badges.py

--- hatsploit/core/cli/badges.py
+++ hatsploit/core/cli/badges.py
@@ -27,37 +27,34 @@
 import os
 
 from hatsploit.core.base.io import IO
-from hatsploit.core.utils.ui.colors_script import ColorsScript
 
 
 class Badges:
     io = IO()
-    colors_script = ColorsScript()
 
-    def print_empty(self, message="", start='\033[1K\r\033[u', end='\n'):
-        line = self.colors_script.parse(message)
+    def print_empty(self, message="", start='%remove', end='%newline'):
         self.io.print(line, start=start, end=end)
 
-    def print_usage(self, message, start='\033[1K\r\033[u', end='\n'):
+    def print_usage(self, message, start='%remove', end='%newline'):
         self.print_empty(f"Usage: {message}", start=start, end=end)
 
-    def print_process(self, message, start='\033[1K\r\033[u', end='\n'):
+    def print_process(self, message, start='%remove', end='%newline'):
         self.print_empty(f"%bold%blue[*]%end {message}", start=start, end=end)
 
-    def print_success(self, message, start='\033[1K\r\033[u', end='\n'):
+    def print_success(self, message, start='%remove', end='%newline'):
         self.print_empty(f"%bold%green[+]%end {message}", start=start, end=end)
 
-    def print_error(self, message, start='\033[1K\r\033[u', end='\n'):
+    def print_error(self, message, start='%remove', end='%newline'):
         self.print_empty(f"%bold%red[-]%end {message}", start=start, end=end)
 
-    def print_warning(self, message, start='\033[1K\r\033[u', end='\n'):
+    def print_warning(self, message, start='%remove', end='%newline'):
         self.print_empty(f"%bold%yellow[!]%end {message}", start=start, end=end)
 
-    def print_information(self, message, start='\033[1K\r\033[u', end='\n'):
+    def print_information(self, message, start='%remove', end='%newline'):
         self.print_empty(f"%bold%white[i]%end {message}", start=start, end=end)
 
     def print_multi(self, message):
-        self.print_empty(f"%bold%blue[*]%end {message}", start='\r', end='')
+        self.print_empty(f"%bold%blue[*]%end {message}", start='%remove', end='')
 
     def input_empty(self, message):
         line = self.colors_script.parse(message)
