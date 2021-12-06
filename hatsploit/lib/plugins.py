ca07eefdd5a50d50d9be54e8e0a62650c7600baf
Ivan Nikolsky <enty8080@gmail.com>
2021-12-06 20:11:31 +0300

    Update plugins.py

--- hatsploit/lib/plugins.py
+++ hatsploit/lib/plugins.py
@@ -83,6 +83,15 @@ class Plugins:
             self.badges.print_error("Failed to load plugin!")
 
     def load_plugin(self, plugin):
+        plugins_shorts = self.local_storage.get("plugin_shorts")
+
+        if plugins_shorts:
+            if plugin.isdigit():
+                plugin_number = int(plugin)
+
+                if plugin_number in plugins_shorts:
+                    plugin = plugins_shorts[plugin_number]
+
         self.badges.print_process("Loading " + plugin + " plugin...")
 
         if not self.check_loaded(plugin):
