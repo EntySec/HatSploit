6bb2a41345648f03af7ad69a6e9aed0c771af8d9
Ivan Nikolsky <enty8080@gmail.com>
2021-12-21 19:42:34 +0100

    Add files via upload

---
+++ hatsploit/modules/auxiliary/generic/scanner/directory_scanner.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+#
+# This module requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.config import Config
+from hatsploit.lib.module import Module
+from hatsploit.utils.http import HTTPClient
+
+
+class HatSploitModule(Module, HTTPClient):
+    config = Config()
+
+    details = {
+        'Name': "WEB Directory Scanner",
+        'Module': "auxiliary/generic/scanner/directory_scanner",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - module developer'
+        ],
+        'Description': "Website directory scanner.",
+        'Platform': "generic",
+        'Rank': "medium"
+    }
+
+    options = {
+        'RHOST': {
+            'Description': "Remote host.",
+            'Value': None,
+            'Type': None,
+            'Required': True
+        },
+        'RPORT': {
+            'Description': "Remote port.",
+            'Value': 80,
+            'Type': "port",
+            'Required': True
+        }
+    }
+
+    def run(self):
+        remote_host, remote_port = self.parse_options(self.options)
+
+        self.print_process(f"Scanning {remote_host}...")
+
+        with open(f"{self.config.path_config['wordlists_path']}directories.txt") as file:
+            directories = list(filter(None, file.read().split('\n')))
+
+        for path in directories:
+            path = '/' + path.replace("\n", "")
+
+            response = self.http_request(
+                method="HEAD",
+                host=remote_host,
+                port=remote_port,
+                path=path
+            )
+
+            if response is not None:
+                if response.status_code == 200:
+                    self.print_success("[%s] ... [%s %s]" % (path, response.status_code, response.reason))
+                else:
+                    self.print_warning("[%s] ... [%s %s]" % (path, response.status_code, response.reason))
---
+++ hatsploit/modules/auxiliary/generic/scanner/port_scanner.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+#
+# This module requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+import time
+
+from hatsploit.lib.module import Module
+from hatsploit.utils.tcp import TCPClient
+
+
+class HatSploitModule(Module, TCPClient):
+    details = {
+        'Name': "Port Scanner",
+        'Module': "auxiliary/generic/scanner/port_scanner",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - module developer'
+        ],
+        'Description': "Scan host for opened ports.",
+        'Platform': "generic",
+        'Rank': "low"
+    }
+
+    options = {
+        'RHOST': {
+            'Description': "Remote host.",
+            'Value': None,
+            'Type': "ip",
+            'Required': True
+        },
+        'RANGE': {
+            'Description': "Ports to scan.",
+            'Value': "0-65535",
+            'Type': "port_range",
+            'Required': True
+        }
+    }
+
+    def check_port(self, remote_host, port):
+        target = remote_host + '/' + str(port)
+        if self.check_tcp_port(remote_host, port):
+            self.print_success(f"{target} - opened")
+
+    def run(self):
+        remote_host, ports_range = self.parse_options(self.options)
+        start, end = self.parse_ports_range(ports_range)
+
+        self.print_process(f"Scanning {remote_host}...")
+        for port in range(start, end):
+            self.check_port(remote_host, port)
