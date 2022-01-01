7546607c307279d2c1cf0783276cd176b97138fa
Ivan Nikolsky <enty8080@gmail.com>
2022-01-01 17:57:28 +0100

    Update sessions.py

--- hatsploit/commands/sessions.py
+++ hatsploit/commands/sessions.py
@@ -8,11 +8,17 @@
 from hatsploit.lib.command import Command
 from hatsploit.lib.sessions import Sessions
 from hatsploit.lib.show import Show
+from hatsploit.lib.config import Config
+from hatsploit.lib.storage import GlobalStorage
 
 
 class HatSploitCommand(Command):
     sessions = Sessions()
     show = Show()
+    config = Config()
+
+    storage_path = config.path_config['storage_path']
+    global_storage = GlobalStorage(storage_path)
 
     usage = ""
     usage += "sessions <option> [arguments]\n\n"
@@ -21,6 +27,7 @@ class HatSploitCommand(Command):
     usage += "  -d, --download <session_id> <remote_file> <local_path>  Download file from session.\n"
     usage += "  -u, --upload <session_id> <local_file> <remote_path>    Upload file to session.\n"
     usage += "  -c, --close <session_id>                                Close specified session.\n"
+    usage += "  --auto-interaction [on|off]                             Interact with session after opening.\n"
 
     details = {
         'Category': "sessions",
@@ -56,5 +63,17 @@ class HatSploitCommand(Command):
                 self.print_usage(self.details['Usage'])
             else:
                 self.sessions.upload_to_session(argv[2], argv[3], argv[4])
+        elif argv[1] == '--auto-interaction':
+            if argc < 3:
+                self.print_usage(self.details['Usage'])
+            else:
+                if argv[2] == 'on':
+                    self.global_storage.set("interact", True)
+                    self.global_storage.set_all()
+                elif argv[2] == 'off':
+                    self.global_storage.set("interact", False)
+                    self.global_storage.set_all()
+                else:
+                    self.print_usage(self.details['Usage'])
         else:
             self.print_usage(self.details['Usage'])
