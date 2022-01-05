070e0edc32eafa65db10ff0651de8ac6f11832c7
Ivan Nikolsky <enty8080@gmail.com>
2022-01-05 01:32:21 +0100

    Update shell_reverse_tcp.py

--- hatsploit/payloads/linux/mipsbe/shell_reverse_tcp.py
+++ hatsploit/payloads/linux/mipsbe/shell_reverse_tcp.py
@@ -8,7 +8,6 @@
 import socket
 
 from hatsploit.lib.payload import Payload
-from hatsploit.utils.tcp import TCPClient
 
 
 class HatSploitPayload(Payload):
@@ -24,21 +23,6 @@ class HatSploitPayload(Payload):
         'Platform': "linux",
         'Rank': "high",
         'Type': "reverse_tcp"
-    }
-
-    options = {
-        'CBHOST': {
-            'Description': "Connect-back host.",
-            'Value': TCPClient.get_local_host(),
-            'Type': "ip",
-            'Required': True
-        },
-        'CBPORT': {
-            'Description': "Connect-back port.",
-            'Value': 8888,
-            'Type': "port",
-            'Required': True
-        }
     }
 
     def run(self):
