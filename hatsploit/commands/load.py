822d200cb0a84a14186e9de2f6460a2ef3a39d4c
Ivan Nikolsky <enty8080@gmail.com>
2022-02-17 23:44:37 +0300

    Update load.py

--- hatsploit/commands/load.py
+++ hatsploit/commands/load.py
@@ -23,5 +23,7 @@ class HatSploitCommand(Command):
         'MinArgs': 1
     }
 
+    complete = plugins.plugins_completer
+
     def run(self, argc, argv):
         self.plugins.load_plugin(argv[1])
