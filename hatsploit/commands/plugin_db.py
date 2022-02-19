b4d1d81ab769b46e13c08f79df086be49d61819b
Ivan Nikolsky <enty8080@gmail.com>
2022-02-19 22:52:45 +0300

    Update plugin_db.py

--- hatsploit/commands/plugin_db.py
+++ hatsploit/commands/plugin_db.py
@@ -16,13 +16,6 @@ class HatSploitCommand(Command):
     builder = Builder()
     show = Show()
 
-    usage = ""
-    usage += "plugin_db <option> [arguments]\n\n"
-    usage += "  -l, --list                        List all connected plugin databases.\n"
-    usage += "  -d, --disconnect <name>           Disconnect specified plugin database.\n"
-    usage += "  -c, --connect <name> <path>       Connect new plugin database.\n"
-    usage += "  -b, --build <path> <output_path>  Build plugin database from plugins path.\n"
-
     details = {
         'Category': "databases",
         'Name': "plugin_db",
@@ -30,28 +23,24 @@ class HatSploitCommand(Command):
             'Ivan Nikolsky (enty8080) - command developer'
         ],
         'Description': "Manage plugin databases.",
-        'Usage': usage,
-        'MinArgs': 1
+        'Usage': "plugin_db <option> [arguments]",
+        'MinArgs': 1,
+        'Options': {
+            '-l': ['', "List all connected plugin databases."],
+            '-d': ['<name>', "Disconnect specified plugin database."],
+            '-c': ['<name> <path>', "Connect new plugin database."],
+            '-b': ['<path> <output_path>', "Build plugin database from plugins path."]
+        }
     }
 
     def run(self, argc, argv):
         choice = argv[1]
-        if choice in ['-l', '--list']:
+
+        if choice == '-l':
             self.show.show_plugin_databases()
-        elif choice in ['-d', '--disconnect']:
-            if argc < 3:
-                self.print_usage(self.details['Usage'])
-            else:
-                self.db.disconnect_plugin_database(argv[2])
-        elif choice in ['-b', '--build']:
-            if argc < 4:
-                self.print_usage(self.details['Usage'])
-            else:
-                self.builder.build_plugin_database(argv[2], argv[3])
-        elif choice in ['-c', '--connect']:
-            if argc < 4:
-                self.print_usage(self.details['Usage'])
-            else:
-                self.db.connect_plugin_database(argv[2], argv[3])
-        else:
-            self.print_usage(self.details['Usage'])
+        elif choice == '-d':
+            self.db.disconnect_plugin_database(argv[2])
+        elif choice == '-b':
+            self.builder.build_plugin_database(argv[2], argv[3])
+        elif choice == '-c':
+            self.db.connect_plugin_database(argv[2], argv[3])
