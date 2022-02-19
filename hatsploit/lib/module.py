f1e5167c6c4ed7e3edca5b2d97766779ebaa662d
Ivan Nikolsky <enty8080@gmail.com>
2022-02-20 00:39:17 +0300

    Update module.py

--- hatsploit/lib/module.py
+++ hatsploit/lib/module.py
@@ -42,6 +42,7 @@ class Module(FMT, Badges, Colors, Parser, Tables, Tools):
         'Description': "",
         'Platform': "",
         'Targets': {},
+        'References': {},
         'Rank': ""
     }
 
