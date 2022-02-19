348e978536e0f64ee2609b9cb3163af5011f9ceb
Ivan Nikolsky <enty8080@gmail.com>
2022-02-19 17:35:14 +0300

    Update ngrok.py

--- hatsploit/plugins/ngrok.py
+++ hatsploit/plugins/ngrok.py
@@ -24,21 +24,18 @@ class HatSploitPlugin(Plugin):
         'Description': "Manage ngrok service right from HatSploit."
     }
 
-    usage = ""
-    usage += "ngrok <option> [arguments]\n\n"
-    usage += "  -l, --list               List all active tunnels.\n"
-    usage += "  -o, --open <port>        Open tunnel for specified port.\n"
-    usage += "  -c, --close <tunnel_id>  Close tunnel by its id.\n"
-    usage += "  -a, --auth <token>       Authenticate ngrok API token.\n"
-
     commands = {
         'ngrok': {
             'ngrok': {
                 'Description': "Manage ngrok service.",
-                'Usage': usage,
+                'Usage': "ngrok <option> [arguments]",
                 'MinArgs': 1,
-                'Options': ['-l', '-o', '-c', '-a', '--list',
-                            '--open', '--close', '--auth']
+                'Options': {
+                    '-l': ['', "List all active tunnels."],
+                    '-o': ['<port>', "Open tunnel for specified port."],
+                    '-c': ['<id>', "Close specified tunnel."],
+                    '-a': ['<token>', "Authenticate ngrok API token."]
+                }
             }
         }
     }
@@ -54,39 +51,30 @@ class HatSploitPlugin(Plugin):
 
     def ngrok(self, argc, argv):
         if argv[1] in ['-o', '--open']:
-            if argc < 2:
-                self.print_usage(self.commands['ngrok']['ngrok']['Usage'])
-            else:
-                try:
-                    self.print_process(f"Opening tunnel for port {argv[2]}...")
+            try:
+                self.print_process(f"Opening tunnel for port {argv[2]}...")
 
-                    tunnel = self.handler.connect(int(argv[2]), "tcp")
-                    data = self.parse_tunnel(tunnel)
+                tunnel = self.handler.connect(int(argv[2]), "tcp")
+                data = self.parse_tunnel(tunnel)
 
-                    route = f"127.0.0.1:{argv[2]} -> {data[0]}:{data[1]}"
+                route = f"127.0.0.1:{argv[2]} -> {data[0]}:{data[1]}"
 
-                    self.print_success(f"Tunnel opened ({route})!")
-                    self.tunnels.append([route, tunnel])
-                except Exception:
-                    self.print_error(f"Failed to open tunnel port {argv[2]}!")
+                self.print_success(f"Tunnel opened ({route})!")
+                self.tunnels.append([route, tunnel])
+            except Exception:
+                self.print_error(f"Failed to open tunnel port {argv[2]}!")
 
         elif argv[1] in ['-a', '--auth']:
-            if argc < 2:
-                self.print_usage(self.commands['ngrok']['ngrok']['Usage'])
-            else:
-                self.handler.set_auth_token(argv[2])
+            self.handler.set_auth_token(argv[2])
 
         elif argv[1] in ['-c', '--close']:
-            if argc < 2:
-                self.print_usage(self.commands['ngrok']['ngrok']['Usage'])
-            else:
-                try:
-                    self.print_process(f"Closing tunnel {argv[2]}...")
+            try:
+                self.print_process(f"Closing tunnel {argv[2]}...")
 
-                    self.handler.disconnect(self.tunnels[int(argv[2])][1])
-                    self.tunnels.pop(int(argv[2]))
-                except Exception:
-                    self.print_error(f"Invalid tunnel given!")
+                self.handler.disconnect(self.tunnels[int(argv[2])][1])
+                self.tunnels.pop(int(argv[2]))
+            except Exception:
+                self.print_error(f"Invalid tunnel given!")
 
         elif argv[1] in ['-l', '--list']:
             headers = ('ID', 'Connection')
