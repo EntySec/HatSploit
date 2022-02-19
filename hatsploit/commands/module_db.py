a23f128af6676d43c2381ad74ad5490411fe87a1
Ivan Nikolsky <enty8080@gmail.com>
2022-02-19 22:49:40 +0300

    Update module_db.py

--- hatsploit/commands/module_db.py
+++ hatsploit/commands/module_db.py
@@ -16,13 +16,6 @@ class HatSploitCommand(Command):
     builder = Builder()
     show = Show()
 
-    usage = ""
-    usage += "module_db <option> [arguments]\n\n"
-    usage += "  -l, --list                        List all connected module databases.\n"
-    usage += "  -d, --disconnect <name>           Disconnect specified module database.\n"
-    usage += "  -c, --connect <name> <path>       Connect new module database.\n"
-    usage += "  -b, --build <path> <output_path>  Build module database from modules path.\n"
-
     details = {
         'Category': "databases",
         'Name': "module_db",
@@ -30,28 +23,24 @@ class HatSploitCommand(Command):
             'Ivan Nikolsky (enty8080) - command developer'
         ],
         'Description': "Manage module databases.",
-        'Usage': usage,
-        'MinArgs': 1
+        'Usage': "module_db <option> [arguments]",
+        'MinArgs': 1,
+        'Options': {
+            '-l': ['', "List all connected module databases."],
+            '-d': ['<name>', "Disconnect specified module database."],
+            '-c': ['<name> <path>', "Connect new module database."],
+            '-b': ['<path> <output_path>', "Build module database from modules path."]
+        }
     }
 
     def run(self, argc, argv):
         choice = argv[1]
-        if choice in ['-l', '--list']:
+
+        if choice == '-l':
             self.show.show_module_databases()
-        elif choice in ['-d', '--disconnect']:
-            if argc < 3:
-                self.print_usage(self.details['Usage'])
-            else:
-                self.db.disconnect_module_database(argv[2])
-        elif choice in ['-b', '--build']:
-            if argc < 4:
-                self.print_usage(self.details['Usage'])
-            else:
-                self.builder.build_module_database(argv[2], argv[3])
-        elif choice in ['-c', '--connect']:
-            if argc < 4:
-                self.print_usage(self.details['Usage'])
-            else:
-                self.db.connect_module_database(argv[2], argv[3])
-        else:
-            self.print_usage(self.details['Usage'])
+        elif choice == '-d':
+            self.db.disconnect_module_database(argv[2])
+        elif choice == '-b':
+            self.builder.build_module_database(argv[2], argv[3])
+        elif choice == '-c':
+            self.db.connect_module_database(argv[2], argv[3])
