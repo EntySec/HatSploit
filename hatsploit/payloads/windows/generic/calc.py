6866e7c0c20ef818ca0b35e8a8b7b108729c8381
Ivan Nikolsky <enty8080@gmail.com>
2022-05-08 15:17:18 +0300

    Update and rename hatsploit/payloads/windows/cmd/calc.py to hatsploit/payloads/windows/generic/calc.py

--- hatsploit/payloads/windows/generic/calc.py
+++ hatsploit/payloads/windows/generic/calc.py
@@ -11,12 +11,12 @@ from hatsploit.lib.payload import Payload
 class HatSploitPayload(Payload):
     details = {
         'Name': "Windows Calculator",
-        'Payload': "windows/cmd/calc",
+        'Payload': "windows/generic/calc",
         'Authors': [
             'Ivan Nikolsky (enty8080) - payload developer'
         ],
         'Description': "Windows calc.exe payload.",
-        'Architecture': "cmd",
+        'Architecture': "generic",
         'Platform': "windows",
         'Rank': "high",
         'Type': "one_side"
