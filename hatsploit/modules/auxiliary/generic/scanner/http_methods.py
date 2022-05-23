201e52a7af4e9d90825e3120baf4818f7a9654de
Noah Altunian <nbaltunian@gmail.com>
2022-05-23 17:14:59 -0400

    Add untested http_methods module

---
+++ hatsploit/modules/auxiliary/generic/scanner/http_methods.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+#
+# This module requires HatSploit: https://hatsploit.com
+# Current source: https://github.com/EntySec/HatSploit
+#
+
+import urllib.request
+from urllib.error import HTTPError
+
+from hatsploit.lib.module import Module
+
+
+class HatSploitModule(Module):
+    details = {
+        'Category': 'auxiliary',
+        'Name': 'HTTP Methods',
+        'Module': 'auxiliary/generic/scanner/http_methods',
+        'Authors': ['Noah Altunian (naltun) - contributor'],
+        'Description': 'Find supported HTTP methods on a server',
+        'Platform': 'generic',
+        'Rank': 'low',
+    }
+
+    options = {
+        'HOST': {
+            'Description': 'Remote host.',
+            'Value': None,
+            'Type': 'ip',
+            'Required': True,
+        }
+    }
+
+    http_methods = [
+        'CONNECT',
+        'DELETE',
+        'GET',
+        'HEAD',
+        'OPTIONS',
+        'PATCH',
+        'POST',
+        'PUT',
+        'TRACE',
+    ]
+
+    supported_methods = {'80': [], '443': []}
+
+    def run(self):
+        remote_host = self.parse_options(self.options)
+
+        self.print_process(f'Scanning {remote_host}...')
+        for method in self.http_methods:
+            for port in [80, 443]:
+                try:
+                    req = urllib.request.Request(
+                        url=f'http://{remote_host}:{port}', method=method
+                    )
+                    resp = urllib.request.urlopen(req)
+                    if resp.status == 200:
+                        self.supported_methods[str(port)].append(method)
+                except HTTPError:
+                    continue
+
+        print(f'Port 80 Supported Methods: {" ".join(supported_methods["80"])}')
+        print(f'Port 443 Supported Methods: {" ".join(supported_methods["443"])}')
