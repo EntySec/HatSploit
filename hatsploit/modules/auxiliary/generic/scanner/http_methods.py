cf50cdd1a84428af565f68b76b6605efae5ecc01
Noah Altunian <nbaltunian@gmail.com>
2022-05-27 16:33:43 -0400

    Use print_success() for supported methods

--- hatsploit/modules/auxiliary/generic/scanner/http_methods.py
+++ hatsploit/modules/auxiliary/generic/scanner/http_methods.py
@@ -62,10 +62,10 @@ class HatSploitModule(Module):
                     continue
 
         if len(self.supported_methods['80']):
-            print(
+            self.print_success(
                 f'Port 80 Supported Methods: {" ".join(self.supported_methods["80"])}'
             )
         if len(self.supported_methods['443']):
-            print(
+            self.print_success(
                 f'Port 443 Supported Methods: {" ".join(self.supported_methods["443"])}'
             )
