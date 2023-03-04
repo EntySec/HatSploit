4839fc47733820f8516abff34921990ad85fcf5b
Ivan Nikolsky <enty8080@gmail.com>
2023-03-04 14:48:43 +0300

    Update run.py

--- hatsploit/commands/run.py
+++ hatsploit/commands/run.py
@@ -6,6 +6,7 @@ Current source: https://github.com/EntySec/HatSploit
 from hatsploit.lib.command import Command
 from hatsploit.lib.modules import Modules
 from hatsploit.lib.runtime import Runtime
+from hatsploit.lib.sessions import Sessions
 
 
 class HatSploitCommand(Command):
@@ -14,6 +15,7 @@ class HatSploitCommand(Command):
 
         self.modules = Modules()
         self.runtime = Runtime()
+        self.sessions = Sessions()
 
         self.details = {
             'Category': "modules",
@@ -30,25 +32,54 @@ class HatSploitCommand(Command):
             },
         }
 
+    def loop(self):
+        self.print_process("Requesting module to run in cycle...")
+
+        self.modules.run_current_module(cycle=True)
+
     def run(self, argc, argv):
+        current_module = self.modules.get_current_module()
+
         if argc > 1 and argv[1] == '-j':
             self.print_process("Running module as a background job...")
+            job_id = self.jobs.count_jobs()
 
             if argc > 2 and argv[2] == '-c':
-                self.print_process("Requesting module to run in cycle...")
-
-                job_id = self.modules.run_current_module(job=True, cycle=True)
+                self.jobs.create_job(
+                    current_module.details['Name'],
+                    current_module.details['Module'],
+                    self.runtime.check,
+                    [self.loop]
+                )
 
             else:
-                job_id = self.modules.run_current_module(job=True)
+                self.jobs.create_job(
+                    current_module.details['Name'],
+                    current_module.details['Module'],
+                    self.runtime.check,
+                    [self.modules.run_current_module]
+                )
 
             self.print_information(
                 f"Module started as a background job {str(job_id)}."
             )
         elif argc > 1 and argv[1] == '-c':
-            self.print_process("Requesting module to run in cycle...")
+            if argc > 2 and argv[2] == '-j':
+                self.print_process("Running module as a background job...")
+                job_id = self.jobs.count_jobs()
 
-            self.modules.run_current_module(cycle=True)
+                self.jobs.create_job(
+                    current_module.details['Name'],
+                    current_module.details['Module'],
+                    self.runtime.check,
+                    [self.loop]
+                )
+
+                self.print_information(
+                    f"Module started as a background job {str(job_id)}."
+                )
+            else:
+                self.loop()
 
         else:
             self.modules.run_current_module()
